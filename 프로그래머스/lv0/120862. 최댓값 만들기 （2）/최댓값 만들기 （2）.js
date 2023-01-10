7a9cd8eb847a8a19fe3287a4f7973a85787a2f53
Sung-ryung <sryung1225@gmail.com>
2023-01-11 01:10:45 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,7 +1,10 @@
 function solution(numbers) {
-    numbers.sort((a,b) => b-a);
-    let answer = numbers[0]*numbers[1] > numbers[numbers.length-1]*numbers[numbers.length-2] 
-                ? numbers[0]*numbers[1] 
-                : numbers[numbers.length-1]*numbers[numbers.length-2];
+    let answer = [];
+    for(let i=0; i<numbers.length-1; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            answer.push(numbers[i]*numbers[j]);
+        }
+    }
+    answer = Math.max(...answer);
     return answer;
 }
