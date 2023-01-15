5205ec8f62abddc96dad1fe9fa8a3bdb200a8681
Sung-ryung <sryung1225@gmail.com>
2023-01-16 01:05:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (2) - 120864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.07 ms
+메모리: 33.6 MB, 시간: 0.12 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,14 +1,6 @@
 function solution(my_string) {
-    let answer = 0;
-    let arr = [];
-    for(let i=0; i<=my_string.length; i++){
-        // console.log(my_string[i], isNaN(my_string[i]));
-        let num = "";
-        while(!isNaN(my_string[i])){
-            num += my_string[i];
-            i++;
-        }
-        if(num != "") arr.push(num);
-    }
-    return arr.reduce((acc,cur) => acc+Number(cur), 0);
+  return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
+// 정규표현식 패턴 중 \D : 숫자가 아닌 것
+// 정규표현식 패턴 중 + : 1개 이상 다수 반복 => 이어서 탐색 가능하도록
+// “1a2b3c4d123Z".split(/\D+/) => [”1”, “2”, “3”, “4”, “123”]
