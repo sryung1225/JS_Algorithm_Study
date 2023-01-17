c8dbba277f4fc9035e2a0b958579392682d74ed6
Sung-ryung <sryung1225@gmail.com>
2023-01-18 01:51:50 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,10 +1,6 @@
 function solution(n) {
-    let arr = [];
-    let num = 0;
-    while(arr.length !== n && ++num){
-		if(num%3 !== 0 && !(''+num).includes('3')){
-			arr.push(num);
-		}
-	}
-  return arr.pop(); // 마지막 num 반환
+  return [...Array(n*3)] // n*3 크기의 [null, null, ... , null]
+    .map((_, i) => i+1) // [1, 2, 3, ... , n*3]
+    .filter(num => num % 3 !== 0 && !num.toString().includes("3")) // 조건에 해당되지 않는 요소들로 배열 필터링
+    [n-1]; // n번째 요소 반환
 }
