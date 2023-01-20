3a8e6d61c457d3df778f2f715a3892f5fdf51555
Sung-ryung <sryung1225@gmail.com>
2023-01-20 21:04:25 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,12 +1,3 @@
 function solution(a, b) {
-    let gcd = 1;
-    for (let i=1; i<=Math.min(a,b); i++) {
-        if (a%i===0 && b%i===0) gcd = i;
-    }
-    b/=gcd;
-
-    while (b%2===0) b/=2;
-    while (b%5===0) b/=5;
-    
-    return b === 1 ? 1 : 2;   
+	return Number((a/b).toFixed(10)) == a/b ? 1 : 2
 }
