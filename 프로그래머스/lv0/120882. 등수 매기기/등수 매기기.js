892d75ef64b9bef358ad51692b25ce2382c3dd57
Sung-ryung <sryung1225@gmail.com>
2023-01-21 02:21:17 +0900

    [level 0] Title: 등수 매기기, Time: 3.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,10 @@
+function solution(score) {
+    let avgArr = score.map(v => (v[0]+v[1])/2); // 평균값 배열
+    let sortArr = [...avgArr].sort((a,b) => b-a); // (avgArr 복제 후) 평균값을 내림차순 정렬한 배열
+    console.log(avgArr, sortArr);
+    
+    // sortArr와 같은 값을 찾으면 그 위치+1
+    // (indexOf는 중복값이 있어도 첫번째 등장한 값만을 찾음 : 중복 해결)
+    let answer = avgArr.map(v => sortArr.indexOf(v)+1)    
+    return answer;
+}
