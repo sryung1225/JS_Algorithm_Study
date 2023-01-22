b00f209e7313e8af32cd2794c6dbbdec51f3e45e
Sung-ryung <sryung1225@gmail.com>
2023-01-23 04:13:30 +0900

    [unrated] Title: 옹알이 (2), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,12 +1,8 @@
 function solution(babbling) {
-    let answer = 0;
-    const word = ["aya", "ye", "woo", "ma"];
-    
-    babbling.forEach(str => {
-        for(let i=0; i<word.length; i++){
-            str = str.split(word[i]).join(i+1);
-        }
-        if(/^[1-4]+$/.test(str) && !(/(11|22|33|44)/g.test(str))) answer++;
-    })
-    return answer;
+    const regexp1 = /(aya|ye|woo|ma)\1+/; // regexp1.test(str) : str에 같은 옹알이를 두 번 이상 반복한 경우가 포함되어 있음
+    const regexp2 = /^(aya|ye|woo|ma)+$/; // regexp2.test(str) : str이 옹알이들로만 이루어짐
+
+    return babbling.reduce((answer, str) => (
+        !regexp1.test(str) && regexp2.test(str) ? ++answer : answer
+    ), 0);
 }
