8615277d86afd8afab9f79b8b4cf665c4b98f3e5
Sung-ryung <sryung1225@gmail.com>
2023-01-23 04:33:34 +0900

    [level 0] Title: 로그인 성공?, Time: 3.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,11 @@
+function solution(id_pw, db) {
+    let answer = "";
+    console.log(id_pw);
+    db.forEach((account) => {
+        if(account[0] === id_pw[0] && account[1] === id_pw[1]) answer = "login";
+        else if(account[0] === id_pw[0] && account[1] !== id_pw[1]) answer = "wrong pw";
+        else if(account[0] !== id_pw[0] && account[1] !== id_pw[1]) answer = "fail";
+    })
+    
+    return answer;
+}
