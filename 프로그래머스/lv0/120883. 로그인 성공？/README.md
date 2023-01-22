ba94ac99e117b79579f1a1bd9a7d955f7ae8e393
Sung-ryung <sryung1225@gmail.com>
2023-01-23 04:37:58 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/README.md
+++ 프로그래머스/lv0/120883. 로그인 성공？/README.md
@@ -1,10 +1,10 @@
 # [level 0] 로그인 성공? - 120883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120883?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,10 +1,5 @@
 function solution(id_pw, db) {
-    let answer = "";
-    db.forEach((account) => {
-        if(account[0] === id_pw[0] && account[1] === id_pw[1]) answer = "login";
-        else if(account[0] === id_pw[0] && account[1] !== id_pw[1]) answer = "wrong pw";
-        else if(account[0] !== id_pw[0] && account[1] !== id_pw[1]) answer = "fail";
-    })
-    
-    return answer;
+    const [id, pw] = id_pw;
+    const map = new Map(db);
+    return map.has(id) ? (map.get(id) === pw ? "login" : "wrong pw") : "fail";
 }
