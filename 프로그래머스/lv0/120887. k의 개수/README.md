b236f11823d6a9feb56feed6a2d7fe08f45bec23
Sung-ryung <sryung1225@gmail.com>
2023-01-26 01:49:08 +0900

    [level 0] Title: k의 개수, Time: 7.37 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/README.md
+++ 프로그래머스/lv0/120887. k의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] k의 개수 - 120887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 11.91 ms
+메모리: 38.4 MB, 시간: 7.37 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,16 +1,7 @@
 function solution(i, j, k) {
-    let answer = 0;
-    
-    // i~j까지 숫자를 한자리씩 쪼갠 후 탐색
-    for(let x=i; x<=j; x++){
-        let arr = x.toString().split("");
-        // console.log(arr)
-        for(let y of arr){
-            if(k == y){
-                answer++;
-                // console.log(y, answer)
-            }
-        }
+    let answer = "";
+    for(i; i<=j; i++){
+        answer += i;
     }
-    return answer;
+    return answer.split(k).length-1;
 }
