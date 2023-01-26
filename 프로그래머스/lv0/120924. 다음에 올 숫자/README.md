d4d9f02fb65468e445bb57cfdce87537cf5db0e9
Sung-ryung <sryung1225@gmail.com>
2023-01-26 16:29:48 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/README.md
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다음에 올 숫자 - 120924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.03 ms
+메모리: 33.6 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,11 +1,8 @@
 function solution(common) {
-    let answer = 0;
-    let diff = common[1] - common[0]; // 등차수열이라 가정한 후 공차 지정
-    if(common[2] === common[1] + diff){
-        answer = common[common.length-1] + diff;
-    } else { // 등비수열
-        diff = common[1] / common[0];
-        answer = common[common.length-1] * diff;
+    if((common[1] - common[0]) === (common[2] - common[1])){
+        return common.pop() + (common[1] - common[0]);
     }
-    return answer;
+    else {
+        return common.pop() * (common[1] / common[0]);
+    }
 }
