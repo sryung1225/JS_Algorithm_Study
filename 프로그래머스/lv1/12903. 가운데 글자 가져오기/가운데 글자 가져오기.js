4d542fd09bbc3dda26df28548a3cce7eff3f4d92
Sung-ryung <sryung1225@gmail.com>
2023-02-12 02:06:33 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,7 +1,4 @@
 function solution(s) {
-    if(s.length%2 === 0){
-        return s.slice((s.length-1)/2, s.length/2+1);
-    } else {
-        return s[(s.length-1)/2];
-    }
+    if(s.length%2 === 0) return s.substring(s.length/2-1, s.length/2+1);
+    else return s.substring(s.length/2, s.length/2+1);
 }
