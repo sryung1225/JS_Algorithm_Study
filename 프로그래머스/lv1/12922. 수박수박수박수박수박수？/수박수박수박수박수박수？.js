92374d195a5a83d8d004fa2955c9a91f00667fb0
Sung-ryung <sryung1225@gmail.com>
2023-02-12 22:33:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,4 @@
 function solution(n) {
-    let answer = [];
-    for(let i=1; i<=n; i++){
-        if(i%2!==0) answer.push('수');
-        else answer.push('박');
-    }
-    return answer.join('');
+    // repeat은 정수만 받음 : 내림됨 (1.5 -> 1)
+    return '수박'.repeat(n/2) + (n%2 === 1 ? '수' : '');
 }
