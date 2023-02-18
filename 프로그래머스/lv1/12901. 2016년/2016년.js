e55a8c4df2bf0361a5a65c0cc204509ae2985c18
Sung-ryung <sryung1225@gmail.com>
2023-02-18 23:01:53 +0900

    [level 1] Title: 2016년, Time: 0.34 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,15 +1,7 @@
 function solution(a, b) {
-    const dayOfMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 2016이 윤년이므로 2월 29일 존재
-    const dayOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-    
-    let days = 1; // 누적 일자를 담을 days
-    
-    // 총 일자 계산
-    for(let i=0; i<a-1; i++) days += dayOfMonth[i];
-    days += b;
-    
-    // 1/1이 금요일인 것을 감안해 요일 배열을 일요일부터 탐색하도록 +3 해준 뒤, 일주일로 나눔
-    days=(days + 3)%7;
-    
-    return dayOfWeek[days];
+    // solution(5, 24)
+    let date = new Date(2016, a-1, b);
+    // console.log(date.toString()); // ? Tue May 24 2016 00:00:00 GMT+0900 (Korean Standard Time)
+    // console.log(date.toString().slice(0,3)); // ? Tue
+    return date.toString().slice(0,3).toUpperCase();
 }
--- 프로그래머스/lv1/12901. 2016년/README.md
+++ 프로그래머스/lv1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.04 ms
+메모리: 34.4 MB, 시간: 0.34 ms
 
 ### 구분
 
