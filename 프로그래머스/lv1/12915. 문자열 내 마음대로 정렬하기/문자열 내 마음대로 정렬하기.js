d4f9dcf51ab6a109521b39fe7e43d67bca6908f0
Sung-ryung <sryung1225@gmail.com>
2023-02-23 15:50:53 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,20 +1,11 @@
 function solution(strings, n) {
-    // 고전.. 방식
-    let tmp = "";
-    for(let i=0; i<strings.length; i++){
-        for(let j=i+1; j<strings.length; j++){
-            if(strings[i].substr(n, 1) > strings[j].substr(n, 1)){
-                tmp = strings[i];
-                strings[i] = strings[j];
-                strings[j] = tmp;
-            } else if(strings[i].substr(n, 1) === strings[j].substr(n, 1)){
-                if(strings[i] > strings[j]){
-                    tmp = strings[i];
-                    strings[i] = strings[j];
-                    strings[j] = tmp;
-                }
-            }
-        }
-    }
-    return strings
+    let answer = strings.sort((a, b) => {
+        const chr1 = a.substr(n, 1);
+        const chr2 = b.substr(n, 1);
+
+        // 오름차순 정석 : sort((a,b) => a-b) : 값이 음수면 순서 바뀜
+        if (chr1 === chr2) return (a > b) - (a < b); // sort()랑 동일한데 풀어 작성하는 것
+        else return (chr1 > chr2) - (chr1 < chr2);
+    })
+    return answer;
 }
