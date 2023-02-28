25a68b13f2059c2cea966d23900f551712525400
Sung-ryung <sryung1225@gmail.com>
2023-02-28 13:17:00 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,4 @@
 function solution(seoul) {
-    for(let i=0; i<seoul.length; i++){
-        if(seoul[i] === "Kim"){
-            return `김서방은 ${i}에 있다`;
-        }
-    }
+    let i = seoul.indexOf("Kim");
+    return `김서방은 ${i}에 있다`;
 }
