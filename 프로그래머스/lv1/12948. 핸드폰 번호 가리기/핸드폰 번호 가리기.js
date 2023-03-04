5134e21cfe143535f3757983f820652f301a384f
Sung-ryung <sryung1225@gmail.com>
2023-03-04 13:29:35 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,3 @@
-function solution(phone_number) {
-    let answer = "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
-    return answer;
+function solution(phone_number){
+    return phone_number.replace(/\d(?=\d{4})/g, "*");
 }
