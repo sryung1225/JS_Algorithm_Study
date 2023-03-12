763c1b52a2ee563c7ab4064c91cba78fb7581b33
Sung-ryung <sryung1225@gmail.com>
2023-03-12 18:37:20 +0900

    [level 1] Title: 직사각형 별찍기, Time: 44.79 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,13 +2,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    // console.log(a);
-    // console.log(b);
+
+    let tmp = "*".repeat(a);
     for(let i=0; i<b; i++){
-        let tmp = "";
-        for(let j=0; j<a; j++){
-            tmp += "*";
-        }
         console.log(tmp);
     }
 });
