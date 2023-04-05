164e2e04f9c8e9622d44337bd6219167ee0511e1
Sung-ryung <sryung1225@gmail.com>
2023-04-06 00:02:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,12 +1,5 @@
 function solution(numbers) {
     let answer = 0;
-    let newSet = new Set(numbers.sort()); // 중복제거 & 오름차순
-    let newArr = Array.from(newSet); // 다시 배열화
-    for(let i=0; i<=9; i++){
-        if(newArr[i] !== i){
-            answer += i;
-            newArr.unshift("*"); // 배열 길이 맞추기 용도
-        }
-    }
+    answer = 45 - numbers.reduce((acc, cur) => acc + cur, 0);
     return answer;
 }
