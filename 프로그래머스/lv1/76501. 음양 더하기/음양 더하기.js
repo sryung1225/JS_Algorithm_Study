4194d29e8ea292610c4dcd51c612b4c850692462
Sung-ryung <sryung1225@gmail.com>
2023-04-05 23:44:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,10 +1,5 @@
 function solution(absolutes, signs) {
     let answer = 0;
-    for(let i=0; i<absolutes.length; i++){
-        if(signs[i] === true)
-            answer += absolutes[i];
-        else
-            answer -= absolutes[i];
-    }
+    answer = absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
     return answer;
 }
