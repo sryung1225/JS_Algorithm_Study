833639e43d7a52647d1681d3710e345c5a9d730f
Sung-ryung <sryung1225@gmail.com>
2023-04-05 23:38:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,10 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    for(let i=0; i<absolutes.length; i++){
+        if(signs[i] === true)
+            answer += absolutes[i];
+        else
+            answer -= absolutes[i];
+    }
+    return answer;
+}
