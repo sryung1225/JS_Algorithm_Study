9b6eaf107f19a48d7510c6874f3485af682c3be4
Sung-ryung <sryung1225@gmail.com>
2023-04-06 00:04:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,8 @@
 function solution(numbers) {
     let answer = 0;
-    answer = 45 - numbers.reduce((acc, cur) => acc + cur, 0);
+    for(let i=0; i<=9; i++){
+        if(!numbers.includes(i))
+            answer += i;
+    }
     return answer;
 }
