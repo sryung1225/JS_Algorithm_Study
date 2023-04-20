dbfcade957f5139888d1e50f21c391a059bd6a56
Sung-ryung <sryung1225@gmail.com>
2023-04-20 23:33:47 +0900

    [level 1] Title: 예산, Time: 0.11 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,10 +1,6 @@
 function solution(d, budget) {
-    const arr = d.sort((a,b)=>a-b); // 가격 오름차순 정렬
-    let sum = 0;
-    let answer = 0;
-    for(let i of arr){
-        sum += i;
-        if(sum <= budget) answer++;
-    }
-    return answer;
+    d.sort((a, b) => a - b); // 가격 오름차순 정렬
+    while(d.reduce((a, b) => a + b, 0) > budget)
+        d.pop();
+    return d.length;
 }
