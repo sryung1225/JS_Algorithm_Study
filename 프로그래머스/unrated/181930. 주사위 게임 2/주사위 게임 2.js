403d7589592aa21149da8152b681ded55154c2e6
Sung-ryung <sryung1225@gmail.com>
2023-04-28 03:45:20 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,8 @@
+function solution(a, b, c) {
+    if(a !== b && b!== c && c !== a)
+        return a+b+c;
+    else if(a === b && b === c)
+        return (a+b+c)*(Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c, 2))*(Math.pow(a, 3)+Math.pow(b, 3)+Math.pow(c, 3));
+    else 
+        return (a+b+c)*(Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c, 2));
+}
