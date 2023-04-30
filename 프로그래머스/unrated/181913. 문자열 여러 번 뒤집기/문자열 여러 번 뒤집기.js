4c21fb08439a620745adcfdde14fd93a1ade081c
Sung-ryung <sryung1225@gmail.com>
2023-05-01 03:46:34 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 26.05 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(my_string, queries) {
+    let tmp = [];
+    my_string = my_string.split("");
+    for(let i of queries){
+        for(let j=i[1]; j>=i[0]; j--){
+            tmp.push(my_string[j]);
+        }
+        my_string.splice(i[0], i[1]-i[0]+1, tmp.join(""));
+        my_string = my_string.join("").split("");
+        tmp = [];
+    }
+    return my_string.join("");
+}
