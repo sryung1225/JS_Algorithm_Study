a60196e6083b48254a690c5d4a41f8b38a24968a
Sung-ryung <sryung1225@gmail.com>
2023-05-02 23:19:20 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,6 +1,3 @@
 function solution(my_string, is_suffix) {
-    if(my_string.slice(my_string.length - is_suffix.length) === is_suffix)
-        return 1;
-    else 
-        return 0;
+    return my_string.slice(my_string.length - is_suffix.length) === is_suffix ? 1 : 0;
 }
