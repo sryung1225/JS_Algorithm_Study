0ab13a5234d940f26eed1767c5dfb06c37c3e44e
Sung-ryung <sryung1225@gmail.com>
2023-05-03 00:29:02 +0900

    [level 1] Title: 체육복, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,15 +1,23 @@
 function solution(n, lost, reserve) {
-    // 여벌을 가져왔는데 도난도 당한 학생 경우 제거 : filter
-    const lostStu = lost.sort((a,b)=>a-b).filter(v => !reserve.includes(v)); // 도난당한 학생 배열
-    let reseStu = reserve.sort((a,b)=>a-b).filter(v => !lost.includes(v));   // 여벌있는 학생 배열
-    // console.log(lostStu, reseStu);
-    
-    let stu = lostStu.filter((lost) => { // 도난당한 학생 한명씩 확인
-        const lend = reseStu.find(v => Math.abs(v-lost) === 1); // 체육복 빌려줄 수 있는 사람 찾기
-        if(lend)
-            reseStu = reseStu.filter(v => v !== lend); // 찾으면, 빌려준 학생을 목록에서 제외
-        else
-            return lost; // 못찾으면, 다음 차례로
-    })
-    return n - stu.length; // 전체 학생 수 - 체육복이 없는 학생 수
+    let answer = 0;
+    const students = new Array(n).fill(1);
+    lost.forEach(v => students[v-1] -= 1);
+    reserve.forEach(v => students[v-1] += 1);
+
+    for(let i=0; i<n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+            students[i-1]++;
+            students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+            students[i+1]++;
+            students[i]--;
+        }
+    }
+    // console.log(students);
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
+        }
+    }
+    return answer;
 }
