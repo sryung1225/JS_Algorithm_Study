38eeddd1f33f1d75ab0b40a49f10ce9d3f62a058
Sung-ryung <sryung1225@gmail.com>
2023-05-04 22:31:59 +0900

    [unrated] Title: 글자 지우기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/README.md
+++ 프로그래머스/unrated/181900. 글자 지우기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 글자 지우기 - 181900 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181900) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181900?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.20 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,8 +1,3 @@
 function solution(my_string, indices) {
-    indices.sort((a, b) => b-a);
-    let answer = [...my_string];
-    for(let i of indices){
-        answer.splice(i, 1);
-    }
-    return answer.join("");
+    return [...my_string].reduce((acc, cur, i) => acc += indices.includes(i) ? "" : cur, "");
 }
