e96388b5c7f333e47d7815111b2d5710c0ed4c9a
Sung-ryung <sryung1225@gmail.com>
2023-05-05 20:14:29 +0900

    [unrated] Title: 리스트 자르기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -1,12 +1,13 @@
 function solution(n, slicer, num_list) {
+    let [a, b, c] = [...slicer];
     switch(n){
         case 1:
-            return num_list.slice(0, slicer[1]+1);
+            return num_list.slice(0, b+1);
         case 2:
-            return num_list.slice(slicer[0]);
+            return num_list.slice(a);
         case 3:
-            return num_list.slice(slicer[0], slicer[1]+1);
+            return num_list.slice(a, b+1);
         case 4:
-            return num_list.slice(slicer[0], slicer[1]+1).filter((v, i) => i % slicer[2] === 0);
+            return num_list.slice(a, b+1).filter((v, i) => i % c === 0);
     }
 }
