2adea0d1c598752de4db982f16836cddb514e956
Sung-ryung <sryung1225@gmail.com>
2023-05-05 20:11:56 +0900

    [unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/README.md
+++ 프로그래머스/unrated/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=javascript) 
 
 ### 성능 요약
 
-메모리: 32.1 MB, 시간: 0.06 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -1,18 +1,12 @@
 function solution(n, slicer, num_list) {
-    let answer = [];
     switch(n){
-        case 1: // 2,4,6,9,17 - 26.3
-            answer = num_list.slice(0, slicer[1]+1);
-            break;
-        case 2: // 7,8,10,16,18 - 26.3
-            answer = num_list.slice(slicer[0]);
-            break;
+        case 1:
+            return num_list.slice(0, slicer[1]+1);
+        case 2:
+            return num_list.slice(slicer[0]);
         case 3:
-            answer = num_list.slice(slicer[0], slicer[1]+1);
-            break;
+            return num_list.slice(slicer[0], slicer[1]+1);
         case 4:
-            answer = num_list.slice(slicer[0], slicer[1]+1);
-            answer = answer.filter((v, i) => i % slicer[2] === 0);
+            return num_list.slice(slicer[0], slicer[1]+1).filter((v, i) => i % slicer[2] === 0);
     }
-    return answer;
 }
