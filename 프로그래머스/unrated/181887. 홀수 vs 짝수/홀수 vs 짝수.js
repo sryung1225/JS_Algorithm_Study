c85390c7d2f8977569dfc59081f837a3ebe37263
Sung-ryung <sryung1225@gmail.com>
2023-05-07 18:54:49 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,11 +1,6 @@
 function solution(num_list) {
-    let evenSum = 0;
-    let oddSum = 0;
-    for(let i=0; i<num_list.length; i++){
-        if(i%2 === 0)
-            evenSum += num_list[i];
-        else
-            oddSum += num_list[i];
-    }
-    return Math.max(evenSum, oddSum);
+    return Math.max(
+        num_list.reduce((acc, cur, idx) => acc + (idx % 2 === 0 ? cur : 0), 0), 
+        num_list.reduce((acc, cur, idx) => acc + (idx % 2 !== 0 ? cur : 0), 0)
+    );
 }
