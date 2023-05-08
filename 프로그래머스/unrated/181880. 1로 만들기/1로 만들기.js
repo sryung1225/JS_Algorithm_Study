845049d1acb5685e9090b25e209ba1fc636aaaf5
Sung-ryung <sryung1225@gmail.com>
2023-05-09 01:37:01 +0900

    [unrated] Title: 1로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,6 +1,6 @@
 function solution(num_list) {
     let answer = 0;
-    for(let i of num_list){
+    num_list.forEach((i) => {
         while(i > 1){
             if(i%2 === 0)
                 i = i/2;
@@ -8,6 +8,6 @@ function solution(num_list) {
                 i = (i-1)/2;
             answer++;
         }
-    }
+    });
     return answer;
 }
--- 프로그래머스/unrated/181880. 1로 만들기/README.md
+++ 프로그래머스/unrated/181880. 1로 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 1로 만들기 - 181880 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.13 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
