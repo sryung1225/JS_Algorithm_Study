c180bb0d52c3a348b962f363442be7fe1487a174
Sung-ryung <sryung1225@gmail.com>
2023-05-09 17:57:38 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.73 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.8 MB, 시간: 0.66 ms
+메모리: 34.1 MB, 시간: 0.73 ms
 
 ### 구분
 
--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,18 +1,27 @@
+// 주어진 배열 시계방향으로 90도 돌리기
+function transpose(matrix){
+    return matrix.reduce(
+        (result, row) => row.map(
+            (_, i) => [...(result[i] || []), row[i]]
+        ), []);
+}
+// board :            [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+// transpose(board) : [[0,0,0,4,3],[0,0,2,2,5],[0,1,5,4,1],[0,0,0,4,3],[0,3,1,2,1]]
+
 function solution(board, moves) {
+    const stacks = transpose(board).map(row => row.reverse().filter(v => v!==0));
+    // console.log(stacks) // ? [[3,4], [5,2,2], [1,4,5,1], [3,4], [1,2,1,3]]
     let answer = 0;
     let bag = [];
     for(let i of moves){
-        for(let j=0; j<board.length; j++){
-            if(board[j][i-1]){
-                bag.push(board[j][i-1]);
-                board[j][i-1] = 0;
-                if(bag[bag.length-1] === bag[bag.length-2]){
-                    bag = bag.slice(0, bag.length-2);
-                    answer += 2;
-                }
-                break;
-            } 
+        const pop = stacks[i-1].pop();
+        if(!pop) continue;
+        if(pop === bag[bag.length-1]){
+            bag.pop();
+            answer += 2;
+            continue;
         }
+        bag.push(pop);
     }
     return answer;
 }
