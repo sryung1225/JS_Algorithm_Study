8eb2b9c5f32fd556d846fd0ed4b7c2ac20dee010
Sung-ryung <sryung1225@gmail.com>
2023-05-12 15:42:52 +0900

    [unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/README.md
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 빈 배열에 추가, 삭제하기 - 181860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181860?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 33.6 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -1,11 +1,7 @@
 function solution(arr, flag) {
-    let X = [];
-    for(let i in arr){
-        if(flag[i]){
-            X = X.concat(new Array(arr[i]*2).fill(arr[i]));
-        } else {
-            X = X.slice(0, X.length - arr[i]);
-        }
-    }
-    return X;
+    return arr.reduce((acc, cur, i) => 
+                     flag[i]
+                     ? [...acc, ...new Array(cur*2).fill(cur)]
+                     : acc.slice(0, -cur)
+                     , []);
 }
