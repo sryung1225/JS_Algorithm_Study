15ba743dcf108b10ef9ce8271a6fc0f9800e45d9
Sung-ryung <sryung1225@gmail.com>
2023-05-12 14:16:54 +0900

    [unrated] Title: 세 개의 구분자, Time: 16.43 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,19 +1,4 @@
 function solution(myStr) {
-    let answer = [];
-    let tmp = "";
-    [...myStr].forEach(v => {
-        if(v !== "a" && v !== "b" && v !== "c"){
-            tmp += v;
-        } else {
-            answer.push(tmp);
-            tmp = "";
-        }
-    })
-    answer.push(tmp);
-    answer = answer.filter(v => v);
-    if(answer.length === 0){
-        return ["EMPTY"];
-    } else {
-        return answer;
-    }
+    let answer = myStr.split(/a|b|c/).filter(v => v);
+    return answer.length === 0 ? ["EMPTY"] : answer;
 }
