cf8270587d61753b0ab13a611dd97fcbd5752573
Sung-ryung <sryung1225@gmail.com>
2023-05-17 02:24:33 +0900

    [unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/README.md
+++ 프로그래머스/unrated/181837. 커피 심부름/README.md
@@ -1,10 +1,10 @@
 # [unrated] 커피 심부름 - 181837 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,22 +1,10 @@
 function solution(order) {
-    let answer = 0;
-    order.forEach(v => {
-        switch(v){
-            case "iceamericano":
-            case "americanoice":
-            case "hotamericano":
-            case "americanohot":
-            case "americano":
-            case "anything":
-                answer += 4500;
-                break;
-            case "icecafelatte":
-            case "cafelatteice":
-            case "hotcafelatte":
-            case "cafelattehot":
-            case "cafelatte":
-                answer += 5000;
-        }
-    })
-    return answer;
+    return order.reduce((acc, cur) => {
+        if (cur.includes("americano"))
+            return acc + 4500;
+        else if (cur.includes("cafelatte"))
+            return acc + 5000;
+        else if (cur === "anything")
+            return acc + 4500;
+    }, 0);
 }
