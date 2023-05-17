279c5e5ea6ccf377e85d117952a605981f14c154
Sung-ryung <sryung1225@gmail.com>
2023-05-17 12:53:06 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,12 +1,9 @@
 function solution(arr) {
-    let countAll = 0;
-    let countSame = 0;
     for(let i=0; i<arr.length; i++){
         for(let j=i; j<arr.length; j++){
-            countAll++;
-            if(arr[i][j] === arr[j][i])
-                countSame++;
+            if(arr[i][j] !== arr[j][i])
+                return 0;
         }
     }
-    return countAll === countSame ? 1 : 0;
+    return 1;
 }
