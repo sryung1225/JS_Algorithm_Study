9e3e47f4dbaf39be9e04846c84c88b1ae3587920
Sung-ryung <sryung1225@gmail.com>
2023-05-20 21:27:01 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.3 MB, 시간: 0.59 ms
+메모리: 33.5 MB, 시간: 0.15 ms
 
 ### 구분
 
--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,19 +1,12 @@
-function isDivisor(n) {
-    let count = 0;
-    for(let i=1; i<=n; i++){
-        if(n % i === 0)
-            count++;
-    }
-    return count;
-}
-
 function solution(left, right) {
     let answer = 0;
     for(let i=left; i<=right; i++){
-        if(isDivisor(i) % 2 === 0){
-            answer += i;
-        } else {
+        // 제곱근이 정수면 약수의 개수가 홀수다
+        // EX) 16의 제곱근 4 => 16의 약수 1,2,4,8,16
+        if(Number.isInteger(Math.sqrt(i))){
             answer -= i;
+        } else {
+            answer += i;
         } 
     }
     return answer;
