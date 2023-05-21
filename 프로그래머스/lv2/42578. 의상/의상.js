c920cafb80410220002b91d6e1b9bb592e59c343
Sung-ryung <sryung1225@gmail.com>
2023-05-21 21:58:01 +0900

    [level 2] Title: 의상, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -1,14 +1,15 @@
 function solution(clothes) {
-    let types = {};
-    clothes.forEach(v => {
-        types[v[1]] = (types[v[1]] || 0) + 1;
-    })
+    let types = clothes.reduce((obj, v)=> {
+        obj[v[1]] = (obj[v[1]] || 0) + 1;
+        return obj;
+    }, {});
     // console.log(types);
     // { headgear: 2, eyewear: 1 }
     
-    let answer = 1;
-    for(let key in types){
-        answer *= (types[key] + 1);
-    }
+    types = Object.values(types);
+    // console.log(types);
+    // [2, 1]
+    
+    let answer = types.reduce((acc, cur)=> acc*(cur+1), 1);
     return answer - 1;
 }
