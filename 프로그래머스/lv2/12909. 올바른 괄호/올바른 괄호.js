9197d402f9215b7dc44f3f8f83ae499e7f7dc2b6
Sung-ryung <sryung1225@gmail.com>
2023-05-22 23:34:14 +0900

    [level 2] Title: 올바른 괄호, Time: 3.28 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,15 @@
+function solution(s){
+    let count = 0;
+    for(let i=0; i<s.length; i++){
+        if(s[i] === "(")
+            count++;
+        else if(s[i] === ")")
+            count--;
+        if(count < 0)
+            return false;
+    }
+    if(count === 0)
+        return true;
+    else
+        return false;
+}
