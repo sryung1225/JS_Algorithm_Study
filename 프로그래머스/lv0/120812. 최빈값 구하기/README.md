6bc525ac5bc27197c4d2ee78a1a081421be98c8c
Sung-ryung <sryung1225@gmail.com>
2023-05-23 14:16:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/README.md
+++ 프로그래머스/lv0/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.25 ms
+메모리: 33.6 MB, 시간: 0.26 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,18 +1,19 @@
 function solution(array) {
-    if([...new Set(array)].length === 1) 
+    if([...new Set(array)].length === 1)
         return array[0];
     
-    array = array.sort((a, b) => a - b);
-    let arr = [];
-    let tmp = 0;
-    for(let i=0; i<array.length; i++){
-        if(array[i] === array[i+1]){
-            tmp++;
-        } else {
-            arr.push([array[i], tmp]);
-            tmp = 0;
-        }
-    }
-    arr.sort((a, b) => b[1] - a[1]);
+    let obj = {};
+    array.forEach(v => {
+        if(!obj[v])
+            obj[v] = 1;
+        else
+            obj[v]++;
+    }) // { '1': 1, '2': 1, '3': 3, '4': 1 }
+
+    let arr = Object.keys(obj).map(v => 
+       [+v, obj[v]]
+    ).sort((a, b) => b[1] - a[1]);
+    // [ [ 3, 3 ], [ 1, 1 ], [ 2, 1 ], [ 4, 1 ] ]
+
     return arr[0][1] === arr[1][1] ? -1 : arr[0][0];
 }
