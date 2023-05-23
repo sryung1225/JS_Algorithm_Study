a24e35662e95d54ab1846be9451ea7ded7ded815
Sung-ryung <sryung1225@gmail.com>
2023-05-23 14:27:52 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.54 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,7 +1,4 @@
 function solution(array) {
-    if([...new Set(array)].length === 1)
-        return array[0];
-    
     let obj = array.reduce((acc, cur) => ({
         ...acc,
         [cur]: (acc[cur] || 0) + 1,
@@ -11,6 +8,6 @@ function solution(array) {
        [+v, obj[v]]
     ).sort((a, b) => b[1] - a[1]);
     // [ [ 3, 3 ], [ 1, 1 ], [ 2, 1 ], [ 4, 1 ] ]
-
-    return arr[0][1] === arr[1][1] ? -1 : arr[0][0];
+    
+    return arr[0][1] === arr?.[1]?.[1] ? -1 : arr[0][0];
 }
