7148cfa5a1a2540ede6a00ee4f953769e00d80a8
Sung-ryung <sryung1225@gmail.com>
2023-05-23 12:30:11 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.67 ms
+메모리: 33.4 MB, 시간: 0.39 ms
 
 ### 구분
 
--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,8 +2,10 @@ function solution(numbers) {
     let answer = [];
     for(let i=0; i<numbers.length; i++){
         for(let j=i+1; j<numbers.length; j++){
-            answer.push(numbers[i]+numbers[j]);
+            let sum = numbers[i]+numbers[j];;
+            if(!answer.includes(sum))
+                answer.push(sum);
         }
     }
-    return [...new Set(answer)].sort((a, b) => a - b);
+    return answer.sort((a, b) => a - b);
 }
