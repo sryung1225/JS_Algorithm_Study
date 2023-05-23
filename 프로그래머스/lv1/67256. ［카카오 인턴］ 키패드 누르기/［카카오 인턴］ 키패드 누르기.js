8028917d3851747a39762684753cf690f983ef0f
Sung-ryung <sryung1225@gmail.com>
2023-05-23 12:15:40 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.82 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,28 +1,41 @@
-function diff(a, b) {
-    return Math.abs(a-b)%3 + Math.floor(Math.abs(a-b)/3);
+function diff([x1, y1], [x2, y2]){
+    return Math.abs(x2 - x1) + Math.abs(y2 - y1);
 }
 function solution(numbers, hand) {
-    let answer = '';
-    let hands = [10, 12];
-    numbers.forEach(v => {
-        if(v === 0) v = 11;
-        if(v % 3 === 1){
-            answer += "L";
-            hands[0] = v;
-        } else if(v % 3 === 0){
-            answer += "R";
-            hands[1] = v;
-        } else { // v % 3 === 2
-            let diffL = diff(hands[0], v);
-            let diffR = diff(hands[1], v);
-            if(diffL < diffR || (diffL === diffR && hand === "left")){
-                answer += "L";
-                hands[0] = v;
+    let answer = "";
+    const keys = [
+        [1, 3],
+        [0, 0], [1, 0], [2, 0],
+        [0, 1], [1, 1], [2, 1],
+        [0, 2], [1, 2], [2, 2],
+    ];
+    const hands = {
+        L: [0, 3],
+        R: [2, 3],
+    };
+    const updateHands = (side, value) => {
+        answer += side;
+        hands[side] = keys[value];
+    }
+    for(let v of numbers) {
+        if (v % 3 === 1) {
+            updateHands("L", v);
+        }
+        else if (v && v % 3 === 0) {
+            updateHands("R", v);
+        }
+        else if(!v || v % 3) { // 0, 2, 5, 8
+            const diffL = diff(hands.L, keys[v])
+            const diffR = diff(hands.R, keys[v])
+            if(diffL < diffR) {
+                updateHands("L", v);
+            }
+            else if(diffL > diffR){
+                updateHands("R", v);
             } else {
-                answer += "R";
-                hands[1] = v;
+                updateHands(hand === "left" ? "L" : "R", v);
             }
         }
-    });
+    }
     return answer;
 }
