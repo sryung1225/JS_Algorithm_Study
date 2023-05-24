60118ac508090b0ebebfdc2d26f745d6d75a75e5
Sung-ryung <sryung1225@gmail.com>
2023-05-24 13:26:17 +0900

    [level 2] Title: 숫자의 표현, Time: 0.55 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -2,14 +2,15 @@ function solution(n) {
     let answer = 1;
     for(let a=1; a<=Math.floor(n/2); a++){
         let b = Math.floor(Math.sqrt(2*n + a*(a-1)));
-        if(b*(b+1) - a*(a-1) === 2*n)
+        if(b*(b+1) - a*(a-1) === 2*n){
             answer++;
+            // let map = [];
+            // for(let i=a; i<=b; i++) map.push(i);
+            // console.log(map)
+        }    
     }
+    // ? [ 1, 2, 3, 4, 5 ]
+    // ? [ 4, 5, 6 ]
+    // ? [ 7, 8 ]
     return answer;
 }
-
-// a <= b
-// n = (a+b)*(b-a+1)/2
-// 2n = -a*a + a + b*b + b
-// 2n = b(b+1) - a(a-1)
-// b(b+1) = 2n + a(a-1)
