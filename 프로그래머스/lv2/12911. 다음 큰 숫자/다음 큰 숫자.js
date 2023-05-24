78a1d957bae93582fa90a95613ef83931e2a899d
Sung-ryung <sryung1225@gmail.com>
2023-05-24 14:01:56 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,12 +1,9 @@
-function solution(n) {
-    let answer = n+1;
+function solution(n, a=n+1) {
     const count1 = n.toString(2).match(/1/g).length;
-    while(true){
-        let count2 = answer.toString(2).match(/1/g).length;
-        if(count1 === count2){
-            return answer;
-        } else {
-            answer++;
-        }
+    let count2 = a.toString(2).match(/1/g).length;
+    if(count1 === count2){
+        return a;
+    } else {
+        return solution(n, a+1);
     }
 }
