ea26ab6458e66e9b4eaff49d9e4855e060b3be38
Sung-ryung <sryung1225@gmail.com>
2023-05-25 17:36:41 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,12 +1,7 @@
 function solution(s) {
-    return s.split(" ").map(v => {
-       v = v.split("").map((k, i) => {
-           if(i % 2 === 0)
-               k = k.toUpperCase();
-           else
-               k = k.toLowerCase();
-           return k;
-       }).join("");
-        return v;
-    }).join(" ");
+    return s.split(" ").map(v => 
+        v.split("").map((k, i) =>
+           i % 2 === 0 ? k.toUpperCase() : k.toLowerCase()
+       ).join("")
+    ).join(" ");
 }
