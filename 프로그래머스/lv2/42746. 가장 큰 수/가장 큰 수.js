0e60178580fbf65e5a8f76cd32aeafbcfc17b110
Sung-ryung <sryung1225@gmail.com>
2023-05-26 23:48:49 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,11 +1,4 @@
 function solution(numbers) {
-    // 오름차순 정렬
-    let answer = numbers.sort((a, b) => {
-        a+="";
-        b+="";
-        return +(b+a) - +(a+b);
-    }).join("");
-    // ex) [30, 311] => 31130 30311 비교해서 양수 => [311, 30]
-    
-    return answer[0] === "0" ? "0" : answer; // [0, 0, 0] 대응
+    let answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");  
+    return answer[0] === "0" ? "0" : answer;
 }
