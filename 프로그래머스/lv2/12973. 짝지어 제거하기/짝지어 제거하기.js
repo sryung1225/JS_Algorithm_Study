e7902256ca8b06c485940a413588bc70970388f7
Sung-ryung <sryung1225@gmail.com>
2023-05-27 23:59:42 +0900

    [level 2] Title: 짝지어 제거하기, Time: 36.36 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,12 +1,11 @@
 function solution(s) {
     const stack = [];
-    for(let i=0; i<s.length; i++){
-        stack.push(s[i])
+    for(let i of s){
+        stack.push(i)
         if(stack[stack.length-1] == stack[stack.length-2] ){
             stack.pop()
             stack.pop()
         }
-        // console.log(s, stack)
     }
     return stack.length === 0 ? 1 : 0;
 }
