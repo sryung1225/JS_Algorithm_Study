522fb1652a5fd3791677d786618c253c3ff71102
Sung-ryung <sryung1225@gmail.com>
2023-05-30 16:09:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,8 +1,13 @@
 function solution(s) {
-    const WORD = ["zero", "one", "two", "three", "four", 
-              "five", "six", "seven", "eight", "nine"];
-    for(let i=0; i<=9; i++){
-        s = s.split(WORD[i]).join(i);
-    }
+    s = s.replace(/zero/gi, 0)
+    .replace(/one/gi, 1)
+    .replace(/two/gi, 2)
+    .replace(/three/gi, 3)
+    .replace(/four/gi, 4)
+    .replace(/five/gi, 5)
+    .replace(/six/gi, 6)
+    .replace(/seven/gi, 7)
+    .replace(/eight/gi, 8)
+    .replace(/nine/gi, 9);
     return +s;
 }
