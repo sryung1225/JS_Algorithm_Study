0cb8afa4d6a171e57fc24fbb62f90d800d8ad34e
Sung-ryung <sryung1225@gmail.com>
2023-05-30 15:15:49 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,6 +1,6 @@
 function solution(t, p) {
     let answer = 0;
-    for(let i=0; i<t.length-p.length+1; i++){
+    for(let i=0; i<=t.length-p.length; i++){
         if(t.slice(i, p.length+i) <= p){
             answer++;
         }
