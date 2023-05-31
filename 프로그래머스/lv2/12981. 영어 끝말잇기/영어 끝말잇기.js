4266f028c92d9baf7fc3c4c8ce3ad45332946539
Sung-ryung <sryung1225@gmail.com>
2023-05-31 15:56:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,9 +1,10 @@
 function solution(n, words) {
     let answer = 0;
-    words.reduce((acc, cur, i) => {
-        if(acc !== cur[0] || words.slice(0, i).indexOf(cur) !== -1)
+    words.reduce((prev, now, i) => {
+        // console.log(prev, now); // 이전 단어의 마지막 글자, 현재 단어
+        if(prev !== now[0] || words.slice(0, i).indexOf(now) !== -1)
             answer = answer || i;
-        return cur[cur.length-1];
+        return now[now.length-1];
     }, "");
     return answer ? [answer%n+1, Math.ceil((answer+1)/n)] : [0, 0];
 }
