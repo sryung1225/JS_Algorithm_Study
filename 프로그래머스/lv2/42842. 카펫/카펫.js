cb970a6c832454572f9bb31731b6b5f7ec9fbd57
Sung-ryung <sryung1225@gmail.com>
2023-05-31 17:13:06 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,10 +1,9 @@
 function solution(brown, yellow) {
     for(let h=1; h<=brown; h++){
-        for(let w=h; w<=brown; w++){
-            let yellowSize = (h === 0) ? w-2 : (w-2)*(h-2);
-            if(brown === (w*h - yellowSize) && yellow === yellowSize){
-                return [w, h];
-            }
+        let w = Math.floor((brown+yellow)/h);
+        let yellowSize = (h === 0) ? w-2 : (w-2)*(h-2);
+        if(brown === (w*h - yellowSize) && yellow === yellowSize){
+            return [w, h];
         }
     }
     return "Can't find";
