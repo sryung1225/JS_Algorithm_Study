5a2b70b5b1bf4872d00c9299b1b0aa5b50eb6531
Sung-ryung <sryung1225@gmail.com>
2023-06-03 00:42:17 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,7 +1,11 @@
 function gcd(a, b) {
-    return a % b ? gcd(b, a%b) : b;
+    // 두 수의 최대공약수를 구하는 함수
+    return a % b !== 0 ? gcd(b, a%b) : b;
 }
 
 function solution(arr){
-    return arr.reduce((a,b) => a*b / gcd(a,b));
+    return arr.reduce((a,b) => {
+        // console.log(a, b, a*b, gcd(a,b), a*b/gcd(a,b));
+        return a*b/gcd(a,b);
+    }, 1);
 }
