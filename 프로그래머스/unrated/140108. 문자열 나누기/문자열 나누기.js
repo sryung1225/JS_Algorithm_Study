50cf0ff763fda87516ea9076c99adedfb41cf751
Sung-ryung <sryung1225@gmail.com>
2023-06-03 01:53:29 +0900

    [unrated] Title: 문자열 나누기, Time: 10.00 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -2,14 +2,13 @@ function solution(s) {
     let answer = 0;
     let arr = [...s];
     while(arr.length > 0){
-        let x = arr.shift();
         let count1 = 1; // x가 등장하는 횟수
         let count2 = 0; // x가 아닌 글자가 등장하는 횟수
         let stop = 0;
-        for(let i=0; i<arr.length; i++){
-            if(arr[i] === x) count1++;
+        for(let i=1; i<arr.length; i++){
+            if(arr[i] === arr[0]) count1++;
             else count2++;
-            if(count1 === count2 || i === arr.length - 1){
+            if(count1 === count2 || i === arr.length-1){
                 stop = i;
                 break;
             }
