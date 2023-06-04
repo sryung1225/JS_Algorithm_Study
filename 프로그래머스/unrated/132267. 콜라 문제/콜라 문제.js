46c3c55857f6129e88c0a15b722db1a44d6492d0
Sung-ryung <sryung1225@gmail.com>
2023-06-04 23:58:34 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,9 +1,8 @@
 function solution(a, b, n) {
     let answer = 0;
     while(n >= a){
-        let sell = Math.floor(n/a);
-        answer += sell*b;
-        n = n - sell*a + sell*b;
+        answer = answer + Math.floor(n/a)*b;
+        n = Math.floor(n/a)*b + n%a;
     }
     return answer;
 }
