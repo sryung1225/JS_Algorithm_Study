ae90f5089818dbbfadfb7b01e6c5b4d0f0810e93
Sung-ryung <sryung1225@gmail.com>
2023-06-04 23:45:36 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.47 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,13 +1,11 @@
 function solution(s) {
     let answer = [];
-    let arr = [];
     [...s].forEach((v, i) => {
-        if(arr.includes(v)){
-            answer.push(i - arr.lastIndexOf(v));
+        if(s.slice(0, i).includes(v)){
+            answer.push(i - s.slice(0, i).lastIndexOf(v));
         } else {
             answer.push(-1);
         }
-        arr.push(v);
     })
     return answer;
 }
