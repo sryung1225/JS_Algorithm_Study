bbc7af8a65035bb713fde9851dddd051edc8e951
Sung-ryung <sryung1225@gmail.com>
2023-06-06 19:01:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,17 +1,13 @@
 function solution(survey, choices) {
-    let types = {"R": 0, "T": 0, "F": 0, "C": 0, 
-                 "M": 0, "J": 0, "A": 0, "N": 0};
+    const types = {"R": 0, "T": 0, "F": 0, "C": 0, 
+                   "M": 0, "J": 0, "A": 0, "N": 0};
 
     survey.forEach((v, i) => {
         let score = choices[i] - 4;
         let type = [...v][score < 0 ? 0 : 1];
         types[type] += Math.abs(score);
-    })
+    });
     
-    let answer = "";
-    types["R"] >= types["T"] ? answer += "R" : answer += "T";
-    types["C"] >= types["F"] ? answer += "C" : answer += "F";
-    types["J"] >= types["M"] ? answer += "J" : answer += "M";
-    types["A"] >= types["N"] ? answer += "A" : answer += "N";
-    return answer;
+    const { R, T, C, F, J, M, A, N } = types;
+    return `${R >= T ? 'R' : 'T'}${C >= F ? 'C' : 'F'}${J >= M ? 'J' : 'M'}${A >= N ? 'A' : 'N'}`;
 }
