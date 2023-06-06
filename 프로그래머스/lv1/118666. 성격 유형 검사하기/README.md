a649e08cee0e57d926135ee3c1086183b5deec9f
Sung-ryung <sryung1225@gmail.com>
2023-06-06 18:58:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.33 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.34 ms
+메모리: 33.7 MB, 시간: 0.33 ms
 
 ### 구분
 
--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,20 +1,11 @@
 function solution(survey, choices) {
     let types = {"R": 0, "T": 0, "F": 0, "C": 0, 
                  "M": 0, "J": 0, "A": 0, "N": 0};
+
     survey.forEach((v, i) => {
-        let score = 0;
-        if(choices[i] === 1)
-            types[v.slice(0, 1)] += 3;
-        else if(choices[i] === 2)
-            types[v.slice(0, 1)] += 2;
-        else if(choices[i] === 3) 
-            types[v.slice(0, 1)] += 1;
-        else if(choices[i] === 5)
-            types[v.slice(1)] += 1;
-        else if(choices[i] === 6)
-            types[v.slice(1)] += 2;
-        else if(choices[i] === 7)
-            types[v.slice(1)] += 3;
+        let score = choices[i] - 4;
+        let type = [...v][score < 0 ? 0 : 1];
+        types[type] += Math.abs(score);
     })
     
     let answer = "";
