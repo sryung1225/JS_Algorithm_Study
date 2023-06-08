de9b969d40333cc20b507a58d42dd0e689388cf1
Sung-ryung <sryung1225@gmail.com>
2023-06-08 14:53:20 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,11 +1,11 @@
-function gcf(n, m) { // 최대공약수
-    return m ? gcf(m, n % m) : n;
+function gcd(n, m) { // 최대공약수
+    return m ? gcd(m, n%m) : n;
 }
 
 function lcm(n, m) { // 최소공배수
-    return n * m / gcf(n, m);
+    return n * m / gcd(n, m);
 }
 
 function solution(n, m) {
-    return [gcf(n, m), lcm(n, m)];
+    return [gcd(n, m), lcm(n, m)];
 }
