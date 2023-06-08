2507980292346ff678e8645204aa51d0d67c3dc5
Sung-ryung <sryung1225@gmail.com>
2023-06-08 15:49:42 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [unrated] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,10 +1,8 @@
 function solution(k, score) {
     let scores = [];
-    let answer = [];
-    for(let i=0; i<score.length; i++){
-        scores.push(score[i]);
+    return score.reduce((acc, cur) => {
+        scores.push(cur);
         scores = scores.sort((a, b) => b - a).slice(0, k);
-        answer.push(scores[scores.length-1]);
-    }
-    return answer;
+        return [...acc, Math.min(...scores)];
+    }, []);
 }
