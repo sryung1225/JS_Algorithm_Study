60c1ae768cfab5506b61ac32149b7616736c5514
Sung-ryung <sryung1225@gmail.com>
2023-06-09 15:41:49 +0900

    [unrated] Title: 둘만의 암호, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,6 +1,19 @@
 function solution(s, skip, index) {
-    let words = "abcdefghijklmnopqrstuvwxyz";
-    words = [...words].filter(v => ![...skip].includes(v));
-    
-    return [...s].map(v => words[(words.indexOf(v) + index) % words.length]).join("");
+    // 'a'.charCodeAt() = 97
+    // 'z'.charCodeAt() = 122
+
+    let answer = "";
+    [...s].map(v => {
+        let a = v.charCodeAt();
+        for(let i=0; i<index; i++){
+            a++;
+            if(a > 122) a = 97;
+            while(skip.includes(String.fromCharCode(a))){
+                a++;
+                if(a > 122) a = 97;
+            }
+        }
+        answer += String.fromCharCode(a);
+    })
+    return answer;
 }
