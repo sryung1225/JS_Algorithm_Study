fdcfa50e02eecf88157ebf72f26d44d9cdc9f3c9
Sung-ryung <sryung1225@gmail.com>
2023-06-10 21:16:17 +0900

    [unrated] Title: 과일 장수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/README.md
+++ 프로그래머스/unrated/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [unrated] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,8 +1,5 @@
 function solution(k, m, score) {
-    let answer = 0;
-    score.sort((a, b) => b - a);
-    for(let i=m-1; i<score.length; i+=m){
-        answer += score[i]*m;
-    }
-    return answer;
+    return score.sort((a, b) => b - a)
+        .filter((_, i) => i%m === m-1)
+        .reduce((acc, cur) => acc + cur*m, 0);
 }
