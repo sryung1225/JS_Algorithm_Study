31eaf61070653742a8ae17ea58ab424931bf3af3
Sung-ryung <sryung1225@gmail.com>
2023-06-10 21:44:56 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,5 +1,5 @@
 function solution(lottos, win_nums) {
-    let dontknow = lottos.filter(v => v === +"0").length;
+    let dontknow = lottos.filter(v => !v).length;
     let same = lottos.filter(v => win_nums.includes(v)).length;
     const rank = n => n >= 2 ? 7 - n : 6;
     return [rank(same+dontknow), rank(same)];
