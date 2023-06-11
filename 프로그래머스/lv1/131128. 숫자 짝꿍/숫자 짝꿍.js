4a486554dec71ffc9905900499a3ecfc8efc9622
Sung-ryung <sryung1225@gmail.com>
2023-06-12 01:21:49 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,22 +1,20 @@
 function solution(X, Y) {
     let common = "";
     let countX = {};
-    [...X].forEach(v => {
-        if(!countX[v])
-            countX[v] = 1;
-        else
-            countX[v]++;
-    });
-    [...Y].forEach(v => {
+    for(let v of X){
+        countX[v] = (countX[v] || 0) + 1;
+    }
+    for(let v of Y){
         if(countX[v] > 0){
             countX[v]--;
             common += v;
         }
-    });
-    if(common === "")
+    }
+    if(common === ""){
         return "-1";
-    else if (+common === 0)
+    } else if (+common === 0){
         return "0";
-    else
-        return [...common].sort((a, b)=> b - a).join("");
+    } else {
+        return [...common].sort((a, b) => b - a).join("");
+    }
 }
