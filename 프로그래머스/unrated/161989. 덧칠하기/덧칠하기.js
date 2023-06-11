8c32afe01b5cc6298f4a223b7b7d4683514f7b58
Sung-ryung <sryung1225@gmail.com>
2023-06-11 23:57:25 +0900

    [unrated] Title: 덧칠하기, Time: 2.39 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,13 @@
 function solution(n, m, section) {
     let answer = 0;
-    let wall = Array(n).fill(0).map((_, i) => section.includes(i+1) ? -1 : 0);
-    for(let i=0; i<wall.length; i++){
+    let wall = Array(n).fill(0);
+    section.forEach(v =>{
+        wall[v-1] = -1;
+    });
+    for(let i=0; i<n; i++){
         if(wall[i] === -1){
             answer++;
-            for(let j=i; j<i+m; j++){
-                wall[j]++;
-            }
+            wall.fill(0, i, i+m);
             i+=m-1;
         }
     }
