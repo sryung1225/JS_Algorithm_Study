c5a3be5a41aa7d574ce8533e2a196b0c585da7f1
Sung-ryung <sryung1225@gmail.com>
2023-06-11 23:41:56 +0900

    [unrated] Title: 덧칠하기, Time: 4723.27 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,13 @@
+function solution(n, m, section) {
+    let answer = 0;
+    let wall = Array(n).fill(0).map((_, i) => section.includes(i+1) ? -1 : 0);
+    for(let i=0; i<wall.length; i++){
+        if(wall[i] === -1){
+            answer++;
+            for(let j=i; j<i+m; j++){
+                wall[j]++;
+            }
+        }
+    }
+    return answer;
+}
