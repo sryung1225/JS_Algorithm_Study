e0c2f697b6a6eb1b9407a3e40083ac5598f3a5ca
Sung-ryung <sryung1225@gmail.com>
2023-06-12 23:46:30 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -3,42 +3,19 @@ function solution(new_id) {
     let answer = new_id.toLowerCase();
     
     // 2단계
-    answer = [...answer].filter(v => (
-        v === "-" || v === "_" || v === "."
-        || !isNaN(Number(v))
-        || v.toUpperCase() !== v
-    )).join("");
+    answer = answer.replace(/[^\w-_.]/g, "");
     
     // 3단계
-    let tmp = 0;
-    for(let i=0; i<answer.length; i++){
-        if(answer[i] === ".")
-            tmp++;
-        else if(answer[i] !== "." && tmp > 0){
-            answer = answer.slice(0, i-tmp+1) + answer.slice(i);
-            i -= tmp-1;
-            tmp = 0;   
-        }
-        if(i === answer.length-1 && tmp > 0){
-            answer = answer.slice(0, i-tmp+2);
-        }
-    }
+    answer = answer.replace(/\.+/g, ".");
     
     // 4단계
-    if(answer[0] === ".")
-        answer = answer.slice(1);
-    if(answer[answer.length-1] === ".")
-        answer = answer.slice(0, answer.length-1);
+    answer = answer.replace(/^\.|\.$/g, "");
     
     // 5단계
-    if(!answer)
-        answer = "a";
+    answer = answer.replace(/^$/, "a");
     
     // 6단계
-    if(answer.length >= 16)
-        answer = answer.slice(0, 15);
-    if(answer[answer.length-1] === ".")
-        answer = answer.slice(0, answer.length-1);
+    answer = answer.slice(0, 15).replace(/\.$/, "");
     
     // 7단계
     if(answer.length <= 2)
