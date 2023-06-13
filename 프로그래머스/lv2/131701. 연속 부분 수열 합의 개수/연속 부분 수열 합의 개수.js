e61e9881d3b4c414e93f2e9baea9569f62ac2fce
Sung-ryung <sryung1225@gmail.com>
2023-06-13 15:28:16 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 92.33 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,8 +2,9 @@ function solution(elements) {
     let ele = elements.concat(elements);
     let answer = new Set();
     for(let i=0; i<elements.length; i++){
+        let sum = 0;
         for(let j=0; j<elements.length; j++){
-            let sum = ele.slice(i, i+j).reduce((acc, cur) => acc + cur, 0);
+            sum += ele[i+j];
             answer.add(sum);
         }
     }
