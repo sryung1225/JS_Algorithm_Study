61c799f1d4f3df9307370cbe587b202622126d70
Sung-ryung <sryung1225@gmail.com>
2023-06-14 17:50:47 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.02 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=javascript) 
 
 ### 성능 요약
 
-메모리: 44 MB, 시간: 10.20 ms
+메모리: 43.9 MB, 시간: 6.02 ms
 
 ### 구분
 
--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,14 +1,9 @@
 function solution(n, left, right) {
-    let arr = new Array(right-left+1).fill(0).reduce((acc, _, idx) => {
-        let i = parseInt((idx+left)/n);
-        let j = (idx+left)%n;
-        acc.push(Math.max(i, j)+1);
-        return acc;
-    }, []);
+    let arr = [];
+    for(let idx=left; idx<=right; idx++){
+        let i = parseInt(idx/n);
+        let j = idx%n;
+        arr.push(Math.max(i, j)+1);
+    }
     return arr;
 }
-
-// 02 10 11 12 //
-//  2  3  4  5 // idx+left
-//  0  1  2  3 // idx
-// [3, 2, 2, 3]
