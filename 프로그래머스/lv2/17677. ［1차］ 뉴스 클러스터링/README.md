2d0ffdc67e17d7845bafa618236c494a14a2fbe2
Sung-ryung <sryung1225@gmail.com>
2023-06-14 19:16:04 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.38 ms
+메모리: 33.6 MB, 시간: 0.37 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -13,18 +13,19 @@ function cutWord(str) {
 function solution(str1, str2) {
     let answer = 0;
     const arr1 = cutWord(str1);
-    let arr2 = cutWord(str2);
+    const arr2 = cutWord(str2);
+    const set = new Set([...arr1, ...arr2]);
+    let uni = 0; // 합집합 갯수
+    let int = 0; // 교집합 갯수
     
-    let max = arr1.length+arr2.length;
-    if(max === 0)
-        return 1*65536;
-    let hab = 0;
-    arr1.forEach(v => {
-        if(arr2.includes(v)){
-            hab++;
-            let idx = arr2.indexOf(v);
-            arr2 = arr2.slice(0, idx).concat(arr2.slice(idx+1));
-        }
+    set.forEach(v => {
+        const has1 = arr1.filter(x => x === v).length;
+        const has2 = arr2.filter(x => x === v).length;
+        uni += Math.max(has1, has2);
+        int += Math.min(has1, has2);
     })
-    return Math.floor(hab/(max-hab)*65536);
+    if(uni === 0)
+        return 1*65536;
+    else
+        return Math.floor(int/uni*65536);
 }
