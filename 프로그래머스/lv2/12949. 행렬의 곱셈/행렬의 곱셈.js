f633036c88c5bca3418e64751a12589553bd430a
Sung-ryung <sryung1225@gmail.com>
2023-06-16 18:45:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.75 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -9,3 +9,10 @@ function solution(arr1, arr2) {
         )
     return answer;
 }
+
+// answer[0][0]
+// - sum = 0
+// - cell = arr1[0][0], arr2[i][j] = arr2[0][0]
+//   - sum = arr1[0][0]*arr2[0][0]
+// - cell = arr1[0][1], arr2[i][j] = arr2[1][0]
+//   - sum = arr1[0][0]*arr2[0][0] + arr1[0][1]*arr[1][0]
