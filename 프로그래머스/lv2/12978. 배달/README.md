b15b43487fefbdec3f2b33ab6d2e52e0bce78766
Sung-ryung <sryung1225@gmail.com>
2023-06-17 01:39:12 +0900

    [level 2] Title: 배달, Time: 4.15 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/README.md
+++ 프로그래머스/lv2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978#qna) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 0.64 ms
+메모리: 37.3 MB, 시간: 4.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,23 +1,21 @@
 function solution(N, road, K) {
-    let arr = Array(N+1).fill(Number.MAX_SAFE_INTEGER);
-    let lines = Array.from(Array(N+1), () => []);
+    const map = Array.from({ length: N }, () => Array(N).fill(Number.MAX_SAFE_INTEGER));
     road.forEach(v => {
-        let [a, b, c] = v;
-        lines[a].push({ to: b, cost: c });
-        lines[b].push({ to: a, cost: c });
+        const [i1, i2, dis] = [v[0]-1, v[1]-1, v[2]];
+        if(dis < map[i1][i2]){
+            map[i1][i2] = dis;
+            map[i2][i1] = dis;
+        }
     });
-
-    let queue = [{ to: 1, cost: 0 }];
-    arr[1] = 0;
-
-    while(queue.length){
-        let { to } = queue.pop();
-        lines[to].forEach((next) => {
-            if (arr[next.to] > arr[to] + next.cost) {
-                arr[next.to] = arr[to] + next.cost;
-                queue.push(next);
+    for(let k=0; k<N; k++){
+        for(let i=0; i<N; i++){
+            for(let j=0; j<N; j++){
+                if(map[i][j] > map[i][k]+map[k][j]){
+                    map[i][j] = map[i][k]+map[k][j];
+                }
             }
-        });
+            map[i][i] = 0;
+        }
     }
-    return arr.filter(v => v <= K).length;
+    return map[0].filter(n => n <= K).length;
 }
