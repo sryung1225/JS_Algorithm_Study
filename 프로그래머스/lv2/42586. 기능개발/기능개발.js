896697fb138e859d52b4e9682336a2b69f197f3d
Sung-ryung <sryung1225@gmail.com>
2023-06-17 03:02:41 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,14 +1,18 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((v, i) => Math.ceil((100 - v)/speeds[i]));
-    let maxDay = days[0];
-    for(let i=0, j=0; i<progresses.length; i++){
-        if(days[i] <= maxDay){
-            answer[j]++;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
+    let answer = [];
+    while(speeds.length){
+        speeds.forEach((v, i) => {
+            if(progresses[i] < 100)
+                progresses[i] += v;
+        })
+        let works = 0;
+        while(progresses[0] >= 100){
+            progresses.shift();
+            speeds.shift();
+            works++;
         }
+        if(works > 0)
+            answer.push(works);
     }
     return answer;
 }
