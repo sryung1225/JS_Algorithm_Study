760bf35b36df4888954a6f7a782094775c149076
Sung-ryung <sryung1225@gmail.com>
2023-06-18 18:29:43 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.60 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.9 MB, 시간: 2.90 ms
+메모리: 33.9 MB, 시간: 1.60 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,9 +1,7 @@
 function solution(n, t, m, p) {
     let arr = "";
-    let i=0;
-    while(arr.length < m*t){
+    for(let i=0; arr.length <= m*t + p; i++){
         arr += i.toString(n).toUpperCase();
-        i++;
     }
     if(p === m) p = 0;
     let answer = [...arr].filter((_, i) => (i+1)%m === p).slice(0, t).join("");
