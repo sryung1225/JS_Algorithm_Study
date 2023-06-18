6a560b35f1f601801c3f67eb8585acb296580eb3
Sung-ryung <sryung1225@gmail.com>
2023-06-18 17:16:21 +0900

    [level 2] Title: [3차] 압축, Time: 0.52 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,26 +1,23 @@
 function solution(msg) {
     let dic = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     let answer = [];
-    let i = 0;
-    while(i<msg.length){
+    
+    for(let i=0; i<msg.length; i++){
         let j = i;
         let [prev, idx] = [0, 0];
         let word = "";
-        while(idx !== -1 && j < msg.length){
-            j++;
-            word = msg.slice(i, j);
+        for(; idx !== -1 && j < msg.length; j++){
+            word = msg.slice(i, j+1);
             prev = idx;
             idx = dic.indexOf(word);
         }
         if(prev !== -1 && idx === -1){
             answer.push(prev);
             dic.push(word);
-            i = j-1;
+            i = j-2;
         } else if(j === msg.length && idx > 0){
             answer.push(idx);
-            i = msg.length;
-        } else {
-            i++;
+            break;
         }
     }
     return answer;
