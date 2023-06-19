c860389de3b81e3af4b0eb8df35e435b7d36df45
Sung-ryung <sryung1225@gmail.com>
2023-06-19 12:39:25 +0900

    [level 2] Title: 할인 행사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,35 @@
+function solution(want, number, discount) {
+    const wantObj = {};
+    want.forEach((v, i) => {
+        if(!discount.includes(v)){
+            return 0;
+        }
+        wantObj[v] = number[i];
+    })
+    
+    let answer = 0;
+    for(let i=0; i<=discount.length-10; i++){
+        const dis = discount.slice(i, i+10);
+        const disObj = {};
+        dis.forEach(v => {
+            if(!disObj[v]){
+                disObj[v] = 1;
+            } else {
+                disObj[v]++;
+            }
+        })
+        
+        let check = 0;
+        for(let item in wantObj){
+            if(!disObj[item] || wantObj[item] > disObj[item]){
+                break;
+            } else {
+                check++;
+            }
+        }
+        if(check === want.length){
+            answer++;
+        }
+    }
+    return answer;
+}
