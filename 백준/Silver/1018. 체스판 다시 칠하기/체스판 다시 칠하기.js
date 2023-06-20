55f02641106355cabb62f48902b3e12b5dda43f9
Sung-ryung <sryung1225@gmail.com>
2023-06-21 00:29:27 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 188 ms, Memory: 9780 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -8,34 +8,30 @@ solution(h, w, inputArr);
 // ------------------------------------------------------------------
 
 function solution(h, w, inputArr) {
-  let chessW = Array.from(Array(8), () => Array(8).fill(""));
+  let chess = Array.from(Array(8), () => Array(8).fill(""));
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       if ((i % 2 === 0 && j % 2 === 0) || (i % 2 === 1 && j % 2 === 1)) {
-        chessW[i][j] = "W";
+        chess[i][j] = "W";
       } else {
-        chessW[i][j] = "B";
+        chess[i][j] = "B";
       }
     }
   }
-  let chessB = chessW.map((v) => v.map((k) => (k === "W" ? "B" : "W")));
 
   let min = 64;
   for (let x = 0; x <= h - 8; x++) {
     for (let y = 0; y <= w - 8; y++) {
-      let comW = 0;
-      let comB = 0;
+      let diff = 0;
       for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-          if (chessW[i][j] !== inputArr[x + i][y + j]) {
-            comW++;
-          } else if (chessB[i][j] !== inputArr[x + i][y + j]) {
-            comB++;
+          if (chess[i][j] !== inputArr[x + i][y + j]) {
+            diff++;
           }
         }
       }
-      if (Math.min(comW, comB) < min) {
-        min = Math.min(comW, comB);
+      if (Math.min(diff, 64 - diff) < min) {
+        min = Math.min(diff, 64 - diff);
       }
     }
   }
