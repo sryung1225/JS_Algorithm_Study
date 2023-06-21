30d60dc141c442f0b67565cd317793ae5963856f
Sung-ryung <sryung1225@gmail.com>
2023-06-21 22:48:18 +0900

    [level 2] Title: 오픈채팅방, Time: 145.89 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    let user = {}; // uid: name
+    let answer = [];
+    arr.forEach((v, i) => {
+        v = v.split(" ");
+        if(!user[v[1]] || v[2]){
+            user[v[1]] = v[2];
+        }
+    })
+    arr.forEach(v => {
+        v = v.split(" ");
+        if(v[0] === "Enter"){
+            answer.push(`${user[v[1]]}님이 들어왔습니다.`);
+        } else if(v[0] === "Leave"){
+            answer.push(`${user[v[1]]}님이 나갔습니다.`);
+        }
+    })
+    return answer;
+}
