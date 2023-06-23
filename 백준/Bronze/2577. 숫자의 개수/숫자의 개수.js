a172aab62e311d823b7f4b83e4c67854e1566327
Sung-ryung <sryung1225@gmail.com>
2023-06-23 23:20:45 +0900

    [Bronze II] Title: 숫자의 개수, Time: 124 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -1,17 +1,16 @@
 const fs = require("fs");
-const input = fs.readFileSync("dev/stdin").toString().trim().split("\n").map((v) => +v);
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map((v) => +v);
 
 solution(input);
 
 // ------------------------------------------------------------------
 
 function solution(input) {
-  let multiple = String(input.reduce((acc, cur) => acc * cur, 1));
+  let multiple = input.reduce((acc, cur) => acc * cur, 1).toString().split("").map((v) => +v);
   const count = {};
-  for (let i = 0; i < multiple.length; i++) {
-    let num = +multiple[i];
-    count[num] = count[num] ? count[num] + 1 : 1;
-  }
+  multiple.forEach((v) => {
+    count[v] = count[v] ? count[v] + 1 : 1;
+  });
   for (let i = 0; i <= 9; i++) {
     console.log(count[i] ? count[i] : 0);
   }
