dd3dbb1fc26184ec219b67e9bcb65b983d83496a
Sung-ryung <sryung1225@gmail.com>
2023-06-23 19:23:20 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.38 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,32 @@
+function solution(files) {
+    let filesDiv = [];
+    files.forEach((v, idx) => {
+        let numberIdx = [];
+        for(let i=0; i<v.length; i++){
+            if(!isNaN(+v[i]) && v[i] !== " "){
+                numberIdx.push(i);
+            }
+            if(numberIdx.length > 0 && isNaN(+v[i])){
+                break;
+            }
+        }
+        let head = v.slice(0, numberIdx[0]).toUpperCase();
+        let number = v.slice(numberIdx[0], numberIdx[numberIdx.length-1]+1);
+        filesDiv.push([head, +number, idx]);
+    })
+    filesDiv.sort((a, b) => {
+        if(a[0] > b[0])
+            return 1;
+        else if(a[0] < b[0])
+            return -1;
+        else if(a[1] !== b[1])
+            return a[1] - b[1];
+        else
+            return 1;
+    });
+    let answer = [];
+    filesDiv.forEach(v => {
+        answer.push(files[v[2]]);
+    })
+    return answer;
+}
