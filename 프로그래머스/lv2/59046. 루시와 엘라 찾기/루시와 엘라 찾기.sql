36b710d3afb3d39b7ed49b103fc261132bab9c75
Sung-ryung <sryung1225@gmail.com>
2023-07-03 16:13:43 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,4 @@
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN ("Lucy", "Ella", "Pickle", "ROGAN", "Sabrina", "Mitty")
+WHERE "Lucy, Ella, Pickle, ROGAN, Sabrina, Mitty" LIKE CONCAT("%", NAME, "%")
 ORDER BY ANIMAL_ID ASC
