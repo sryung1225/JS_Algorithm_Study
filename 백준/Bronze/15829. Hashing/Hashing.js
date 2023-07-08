538fb75de43c8ad68bdd081e19996b3a145e59aa
Sung-ryung <sryung1225@gmail.com>
2023-07-09 00:09:35 +0900

    [Bronze II] Title: Hashing, Time: 152 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const n = +input[0];
+const str = input[1];
+solution(n, str);
+
+// ------------------------------------------------------------------
+
+function solution(n, str) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  let num = [...str].reduce((acc, cur, idx) => {
+    let tmp = alpha.indexOf(cur) + 1;
+    return acc + tmp * Math.pow(31, idx);
+  }, 0);
+  console.log(num % 1234567891);
+}
