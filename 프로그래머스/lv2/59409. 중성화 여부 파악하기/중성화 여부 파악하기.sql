0030a5c55bfc06387a0106a80c39dd104f9aa17e
Sung-ryung <sryung1225@gmail.com>
2023-07-09 22:40:48 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,4 +1,4 @@
 SELECT ANIMAL_ID, NAME, 
-    IF (SEX_UPON_INTAKE REGEXP ("Neutered|Spayed"), "O", "X") AS 중성화
+    IF (SEX_UPON_INTAKE LIKE "%Neutered%" OR SEX_UPON_INTAKE LIKE "%Spayed%", "O", "X") AS 중성화
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID ASC;
