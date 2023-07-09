18bd3c070ef085cf2171f314ad599372c5bcfe88
Sung-ryung <sryung1225@gmail.com>
2023-07-09 18:30:33 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,2 +1,9 @@
-SELECT ANIMAL_TYPE, IFNULL(NAME, "No name"), SEX_UPON_INTAKE
+SELECT ANIMAL_TYPE,
+    CASE
+        WHEN NAME IS NULL 
+        THEN "No name" 
+        else NAME 
+        END
+    NAME, 
+    SEX_UPON_INTAKE
 FROM ANIMAL_INS
