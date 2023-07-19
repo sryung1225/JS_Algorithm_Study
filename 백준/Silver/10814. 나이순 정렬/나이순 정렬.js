e21d8de873e262a65ac837f716195e66c76f7871
Sung-ryung <sryung1225@gmail.com>
2023-07-19 17:35:13 +0900

    [Silver V] Title: 나이순 정렬, Time: 5488 ms, Memory: 27124 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 const n = +input[0];
-const inputArr = input.slice(1).map((v) => v.trim().split(" "));
+const inputArr = input.slice(1);
 
 solution(n, inputArr);
 
 // ------------------------------------------------------------------
 
 function solution(n, inputArr) {
-  inputArr.sort((a, b) => +a[0] - +b[0]);
+  inputArr.sort((a, b) => parseInt(a) - parseInt(b));
   inputArr.forEach((v) => {
-    console.log(+v[0] + " " + v[1]);
+    console.log(v);
   });
 }
