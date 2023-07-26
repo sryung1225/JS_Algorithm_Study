da9836e61bdff5e359d730d403089e0083bd4aed
Sung-ryung <sryung1225@gmail.com>
2023-07-26 13:13:08 +0900

    [level 2] Title: 하노이의 탑, Time: 5.10 ms, Memory: 48.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,13 +1,13 @@
 function solution(n) {
     const answer = [];
     function hanoi(n, start, end, mid){
-        if(n === 1){
+        if(n === 1){ // 원판 갯수가 1개이면 끝으로 바로 옮김 (종료)
             answer.push([start, end]);
-            return NaN;
+        } else {
+            hanoi(n-1, start, mid, end); // 가장 밑 원판 외 원판들을 중간으로 이동
+            answer.push([start, end]);
+            hanoi(n-1, mid, end, start); // 중간으로 이동한 원판들을 목표 위치로 이동
         }
-        hanoi(n-1, start, mid, end);
-        answer.push([start, end]);
-        hanoi(n-1, mid, end, start);
     }
     hanoi(n, 1, 3, 2);
     return answer;
