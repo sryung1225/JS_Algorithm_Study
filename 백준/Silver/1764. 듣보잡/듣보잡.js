51825c6555d77337851b264d789e35000924e813
Sung-ryung <sryung1225@gmail.com>
2023-11-07 19:31:08 +0900

    [Silver IV] Title: 듣보잡, Time: 236 ms, Memory: 26552 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 const [N, M] = input[0].split(" ").map((v) => +v);
 const arrN = input.slice(1, N + 1).map((v) => v.trim());
-const arrMSet = new Set(input.slice(N + 1).map((v) => v.trim()));
+const setM = new Set(input.slice(N + 1).map((v) => v.trim()));
 
-solution(arrN, arrMSet);
+solution(arrN, setM);
 
 // ------------------------------------------------------------------
 
-function solution(arrN, arrMSet) {
-  const strangers = arrN.filter((who) => arrMSet.has(who));
+function solution(arrN, setM) {
+  const strangers = arrN.filter((who) => setM.has(who));
   console.log(strangers.length);
   console.log(strangers.sort().join("\n"));
 }
