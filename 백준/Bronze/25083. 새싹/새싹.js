d4d274454e3f9235e645e28d2837d5a7a73ade8c
Sung-ryung <sryung1225@gmail.com>
2023-11-13 00:48:46 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9288 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.js
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -1,6 +1,6 @@
-console.log(`         ,r'"7
-r\`-_   ,'  ,/
- \\. ". L_r'
-   \`~\\/
-      |
-      |`);
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
