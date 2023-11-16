4c60b55aa069e3050bf34456b5cb0da2acd3f525
Sung-ryung <sryung1225@gmail.com>
2023-11-16 15:55:06 +0900

    [Silver IV] Title: ATM, Time: 200 ms, Memory: 11796 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const N = parseInt(input.shift(), 10);
+
+solution(N, input);
+
+// ------------------------------------------------------------------
+
+function solution(N, input) {
+  const times = input[0].split(" ").map(Number);
+  times.sort((a, b) => a - b);
+
+  let answer = 0;
+  times.forEach((_, index) => {
+    const arr = times.slice(0, index + 1);
+    answer += arr.reduce((acc, cur) => acc + cur, 0);
+  });
+  console.log(answer);
+}
