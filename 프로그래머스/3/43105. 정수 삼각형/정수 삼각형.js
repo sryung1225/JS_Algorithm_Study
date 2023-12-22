b3eb0663226a303cf696097eee89784c645dee08
Sung-ryung <sryung1225@gmail.com>
2023-12-23 00:23:32 +0900

    [level 3] Title: 정수 삼각형, Time: 4.14 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,8 +1,8 @@
 function solution(triangle) {
     for(let x=triangle.length-1; x>=0; x--){
-        for(let y=0; y<triangle[x].length - 1; y++){
-            const temp = Math.max(triangle[x][y], triangle[x][y + 1]);
-            triangle[x-1][y] += temp;
+        for(let y=0; y<triangle[x].length-1; y++){
+            const big = Math.max(triangle[x][y], triangle[x][y+1]);
+            triangle[x-1][y] += big;
         };
     };
     return triangle[0][0];
