ef696208e5700cb8d46b383d9a3df4a1f650a617
Sung-ryung <sryung1225@gmail.com>
2023-12-22 23:59:34 +0900

    [level 1] Title: 소수 찾기, Time: 147.10 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const s = new Set();
+    s.add(2);
+    for(let i=3; i<=n; i+=2){
+        s.add(i);
+    }
+    
+    for(let j=3; j<Math.sqrt(n); j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
+        }
+    }
+    return s.size;
+}
