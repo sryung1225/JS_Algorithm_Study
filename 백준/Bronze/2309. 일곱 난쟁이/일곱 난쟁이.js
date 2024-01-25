097c721bb0b1ff1449e7b8a1c14a640d3f8e79d9
YiRyung <sryung1225@gmail.com>
2024-01-25 20:06:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -10,15 +10,17 @@ const input = fs
 // ------------------------------------------------------------------
 // 7명 합산이 100이 되는 경우 중 하나 찾기
 
-function shuffle(array) {
-  const newArray = [...array].sort(() => Math.random() - 0.5);
-  return newArray;
+const sum = input.reduce((acc, cur) => acc + cur, 0);
+let array = [...input].sort((a, b) => a - b);
+let breakpoint = false;
+for (let i = 0; i < 8; i++) {
+  for (let j = i + 1; j < 9; j++) {
+    if (sum - (array[i] + array[j]) === 100) {
+      array = array.filter((_, idx) => idx !== i && idx !== j);
+      breakpoint = true;
+      break;
+    }
+  }
+  if (breakpoint) break;
 }
-let sum = 0;
-let newArr = [];
-while (sum !== 100) {
-  newArr = shuffle(input).slice(0, 7);
-  sum = newArr.reduce((acc, cur) => acc + cur, 0);
-}
-newArr.sort((a, b) => a - b);
-console.log(newArr.join("\n"));
+console.log(array.join("\n"));
