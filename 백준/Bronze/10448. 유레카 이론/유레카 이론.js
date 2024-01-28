1b38d15017653cf3efd3d667c8fa654432952ab7
YiRyung <sryung1225@gmail.com>
2024-01-29 00:46:15 +0900

    [Bronze I] Title: 유레카 이론, Time: 260 ms, Memory: 11924 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "run/input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+const N = input.shift();
+
+// ------------------------------------------------------------------
+
+function tri(n) {
+  return (n * (n + 1)) / 2;
+}
+
+function solution(K) {
+  let numArr = [];
+  for (let i = 1; tri(i) <= K; i++) {
+    numArr.push(tri(i));
+  }
+  for (let i = 0; i < numArr.length; i++) {
+    for (let j = 0; j < numArr.length; j++) {
+      for (let k = 0; k < numArr.length; k++) {
+        if (numArr[i] + numArr[j] + numArr[k] === K) {
+          return 1;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+input.forEach((num) => {
+  console.log(solution(num));
+});
