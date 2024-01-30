551d08e52a9b57c4d41e1af210acb7530cf48205
YiRyung <sryung1225@gmail.com>
2024-01-30 21:36:14 +0900

    [Silver V] Title: 폴리오미노, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.js
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.js
@@ -4,38 +4,8 @@ const input = fs.readFileSync(filePath).toString().trim();
 
 // ------------------------------------------------------------------
 
-function transfer(arr) {
-  if (arr.length % 2 !== 0) return -1;
-  let result = "";
-  result += "AAAA".repeat(Math.floor(arr.length / 4));
-  if (arr.length % 4 === 2) result += "BB";
-  return result;
-}
-
-let answer = "";
-let stack = [];
-for (let i = 0; i < input.length; i++) {
-  if (input[i] === "X") {
-    stack.push(input[i]);
-  } else if (input[i] === ".") {
-    const temp = transfer(stack);
-    if (temp === -1) {
-      answer = -1;
-      break;
-    }
-    answer += temp;
-    stack = [];
-    answer += ".";
-  }
-}
-
-if (answer !== -1 && stack.length !== 0) {
-  const temp = transfer(stack);
-  if (temp === -1) {
-    answer = -1;
-  } else {
-    answer += temp;
-  }
-}
-
+let answer = input;
+answer = answer.replace(/XXXX/g, "AAAA");
+answer = answer.replace(/XX/g, "BB");
+answer = answer.split("").includes("X") ? -1 : answer;
 console.log(answer);
