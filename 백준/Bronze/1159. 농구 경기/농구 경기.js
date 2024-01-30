7ffa97ca7cee32d9027b423873ee54d1e07caf5e
YiRyung <sryung1225@gmail.com>
2024-01-30 20:57:57 +0900

    [Bronze II] Title: 농구 경기, Time: 156 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "run/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const N = input.shift();
+
+// ------------------------------------------------------------------
+
+const names = {};
+input.forEach((name) => {
+  if (!names[name[0]]) {
+    names[name[0]] = [];
+  }
+  names[name[0]].push(name.trim());
+});
+
+const answer = [];
+Object.values(names).forEach((arr) => {
+  if (arr.length >= 5) {
+    answer.push(arr[0][0]);
+  }
+});
+answer.sort();
+console.log(answer.length !== 0 ? answer.join("") : "PREDAJA");
