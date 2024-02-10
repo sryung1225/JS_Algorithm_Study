e5b742f9c0355a3cb52879714ddc49b6f602c231
YiRyung <sryung1225@gmail.com>
2024-02-11 01:46:28 +0900

    [Silver III] Title: 햄버거 분배, Time: 196 ms, Memory: 10216 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.js
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.js
@@ -8,11 +8,10 @@ let str = input[1].split("");
 
 let answer = 0;
 for (let i = 0; i < N; i++) {
-  const v = str[i];
-  if (v === "H" || v === "-") continue;
+  if (str[i] === "H" || str[i] === "-") continue;
   for (let j = Math.max(i - K, 0); j <= Math.min(i + K, N - 1); j++) {
     if (str[j] === "H") {
-      str.splice(j, 1, "-");
+      str[j] = "-";
       answer++;
       break;
     }
