6136df660166f5628bb4c20b91bbac5e1bc844b4
YiRyung <sryung1225@gmail.com>
2024-05-08 13:21:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,8 @@
+function solution(left, right) {
+    let answer = 0;
+    for(let i=left; i<=right; i++){
+        if(Math.sqrt(i) % 1 === 0) answer -= i;
+        else answer += i;
+    }
+    return answer;
+}
