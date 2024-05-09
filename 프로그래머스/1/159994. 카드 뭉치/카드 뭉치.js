34a708ac25735b85317dc5e1956417d21a1ba5ea
YiRyung <sryung1225@gmail.com>
2024-05-09 17:06:42 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,15 +1,13 @@
 function solution(cards1, cards2, goal) {
     let answer = "Yes";
+    let [idx1, idx2] = [0, 0];
     for(let i=0; i<goal.length; i++){
-        const word = goal[i];
-        if(word === cards1[0]){
-            cards1.shift();
-        } else if(word === cards2[0]){
-            cards2.shift();
-        } else {
+        if(cards1[idx1] === goal[i]) idx1++;
+        else if(cards2[idx2] === goal[i]) idx2++;
+        else {
             answer = "No";
             break;
         }
-    }  
+    }
     return answer;
 }
