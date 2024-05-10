839b9d4ab92f53c608450af002a9ac1120230a16
YiRyung <sryung1225@gmail.com>
2024-05-10 17:27:37 +0900

    [level 1] Title: 소수 찾기, Time: 138.97 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,16 +1,14 @@
 function solution(n) {
-    const s = new Set();
-    s.add(2);
+    const set = new Set([2]);
     for(let i=3; i<=n; i+=2){
-        s.add(i);
+        set.add(i);
     }
-    
     for(let j=3; j<Math.sqrt(n); j++){
-        if(s.has(j)){
+        if(set.has(j)){
              for(let k=j*2; k<=n; k+=j){    
-                s.delete(k);
+                set.delete(k);
              }
         }
     }
-    return s.size;
+    return set.size;
 }
