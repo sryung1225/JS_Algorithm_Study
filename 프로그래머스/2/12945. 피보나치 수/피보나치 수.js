ddb03a3572ffdaf925c135467d2f024dd5b47481
YiRyung <sryung1225@gmail.com>
2024-05-12 00:36:48 +0900

    [level 2] Title: 피보나치 수, Time: 4.74 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,7 +1,9 @@
+let F = [0, 1];
+
 function solution(n) {
-    let restFibo = [0, 1];
-    for(let i=2; i<=n; i++){
-        restFibo.push((restFibo[i-2] + restFibo[i-1]) % 1234567);
+    if(F.length > n) return F[n];
+    for(let i=F.length; i<=n; i++) {
+        F[i] = (F[i-2] + F[i-1]) % 1234567;
     }
-    return restFibo[n];
+    return F[n];
 }
