ee648a2852fa62f3801ff244b0f2690338a46885
YiRyung <sryung1225@gmail.com>
2024-05-12 01:08:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.91 ms, Memory: 46.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s)
+{
+    let stack = [];
+    for(let i=0; i<s.length; i++){
+        stack.push(s[i]);
+        if(stack[stack.length-1] === stack[stack.length-2]){
+            stack.pop();
+            stack.pop();
+        }
+    }
+    return stack.length === 0 ? 1 : 0;
+}
