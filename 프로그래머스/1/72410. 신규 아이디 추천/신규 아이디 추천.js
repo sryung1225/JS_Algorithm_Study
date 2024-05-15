cc6248c23d51fca216f5bef7fcbe83f39e1422ec
YiRyung <sryung1225@gmail.com>
2024-05-15 21:52:22 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,28 @@
+function solution(new_id) {
+    const s = "abcdefghijklmnopqrstuvwxyz0123456789-_.";
+    let id1 = new_id.toLowerCase();
+    let id2 = id1.split("").filter((v) => s.includes(v));
+    let id3 = '';
+    for(let i=0; i<id2.length; i++){
+        if(id2[i] === "." && id2[i+1] === ".") continue;
+        id3 += id2[i];
+    }
+    let id4_1 = id3[0] === "." ? id3.slice(1) : id3;
+    let id4_2 = id4_1[-1] === "." ? id4_1.slice(id4_1.length - 2) : id4_1;
+    let id5 = id4_2.length === 0 ? "a" : id4_2;
+    let id6_1 = id5.slice(0, 15);
+    let id6_2 = '';
+    for(let i=id6_1.length - 1; i>=0; i--){
+        if(id6_1[i] !== "."){
+            id6_2 = id6_1.slice(0, i+1);
+            break;
+        }
+    }
+    let answer = id6_2;
+    if(answer.length <= 2){
+        while(answer.length < 3){
+            answer += answer[answer.length - 1];
+        }
+    }
+    return answer;
+}
