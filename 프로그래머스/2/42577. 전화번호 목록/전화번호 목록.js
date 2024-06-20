49ab26383cafe5db60b57855c83cec7d8e4ecd1c
YiRyung <sryung1225@gmail.com>
2024-06-20 12:57:46 +0900

    [level 2] Title: 전화번호 목록, Time: 186.26 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,12 +1,12 @@
 function solution(phone_book) {
-    phone_book.sort(); // 사전 순 정렬
-    for(let i=0; i<phone_book.length - 1; i++){
-        const number1 = phone_book[i];
-        const size = number1.length;
-        const number2 = phone_book[i+1];
-        const pre = number2.slice(0, size);
-        if(number1 === pre){
-            return false;
+    const set = new Set(phone_book);
+    for(let i=0; i<phone_book.length; i++) {
+        const number = phone_book[i];
+        for(let j=1; j<number.length; j++) {
+            const pre = number.slice(0, j);
+            if(set.has(pre)) {
+                return false;
+            }
         }
     }
     return true;
