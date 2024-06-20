88ae5726d37fbcb16f7f77762798052c1d6d148a
YiRyung <sryung1225@gmail.com>
2024-06-21 01:48:28 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,27 +1,22 @@
 function isPrime(num) {
     const sqrtNum = Math.sqrt(num);
-    if(Number.isInteger(sqrtNum)){
-        return false;
-    }
+    
+    if(Number.isInteger(sqrtNum)) return false;
     for(let i=2; i<sqrtNum; i++){
-        if(num % i === 0){
-            return false;
-        }
+        if(num % i === 0) return false;
     }
     return true;
 }
 
 function solution(n, k) {
     const num = n.toString(k);
-    const arr = num.split("0");
+    const arr = num.split("0").map(Number);
     
     let answer = 0;
     
     arr.forEach((p) => {
-        const check = isPrime(p);
-        if(check){
-            answer++;
-        }
+        if(p === '') return;
+        if(isPrime(p)) answer++;
     });
     
     return answer;
