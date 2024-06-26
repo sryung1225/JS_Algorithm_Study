5cfa633db60ef79bebe348b7add1ca216bd74f2b
YiRyung <sryung1225@gmail.com>
2024-06-26 16:02:37 +0900

    [level 2] Title: 주식가격, Time: 5.48 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 42.1 MB, 시간: 4.97 ms
+메모리: 41.9 MB, 시간: 5.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 15:34:16
+2024년 06월 26일 16:02:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,22 @@
 function solution(prices) {
     const answer = new Array(prices.length).fill(0);
-    prices = prices.slice(0, prices.length - 1);
+    const stack = []; // 하락하지 않은 가격의 인덱스들
     
     for(let i=0; i<prices.length; i++){
-        for(let j=i; j<prices.length; j++){
-            if(prices[i] <= prices[j]) {
-                answer[i]++;
-            } else {
-                break;
-            }
+        const price = prices[i]; // 현재 가격
+        // 아직 하락하지 않는 가격들이 있으면서
+        // 그 가격들이 현재 가격보다 높은 가격일 때 (= 지금 하락했을 때)
+        while(stack.length > 0 && prices[stack[stack.length - 1]] > price){
+            const idx = stack.pop();
+            answer[idx] = i - idx; // 현재 인덱스 - 하락 시작 인덱스 (= 가격 유지 시간)
         }
-    } 
+        stack.push(i); // 현재 인덱스 저장
+    }
+    
+    // 스택에 남은 인덱스 = 끝까지 가격을 방어한 인덱스들
+    while (stack.length > 0) {
+        const idx = stack.pop();
+        answer[idx] = prices.length - 1 - idx; // 최대 시간 부여
+    }
     return answer;
 }
