47380bda41d3e28a54ccf376953a4383a9c394cc
YiRyung <sryung1225@gmail.com>
2024-09-11 16:00:33 +0900

    [level 2] Title: 행렬의 곱셈, Time: 30.45 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,11 +1,11 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    for(let i=0; i<arr1.length; i++){
-        let temp = [];
+    const answer = [];
+    for (let i=0; i<arr1.length; i++){
+        const temp = [];
         for(let j=0; j<arr2[0].length; j++){
             let sum = 0;
             for(let k=0; k<arr1[0].length; k++){
-                sum += arr1[i][k]*arr2[k][j];
+                sum += arr1[i][k] * arr2[k][j];
             }
             temp.push(sum);
         }
