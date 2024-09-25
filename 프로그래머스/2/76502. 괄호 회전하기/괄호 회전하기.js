8218b4ad2f6218bac437378a217e0289dfe98a42
YiRyung <sryung1225@gmail.com>
2024-09-25 21:41:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 29.14 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,43 +1,26 @@
-function match(w) {
-    if(w === ")") return "(";
-    if(w === "]") return "[";
-    if(w === "}") return "{";
-    return false;
-}
-
 function solution(s) {
-    const arr = s.split("");
     const size = s.length;
-    // s의 길이가 짝수인 경우 우선 종료
-    if(size % 2 !== 0) return 0;
     let answer = 0;
-    
-    for(let i=0; i<size; i++){
-        let stack = [];
-        let stop = false;
+    let row = s.split("");
+    for(let i = 0; i<size; i++){
+        const stack = [];
         for(let j=0; j<size; j++){
-            const w = arr[j];
-            const m = match(w);
-            if(!m) stack.push(w);
-            else if(m && m === stack[stack.length - 1]) stack.pop();
-            else {
-                stop = true; // 강제 종료 표시
+            const s = row[j];
+            const l = stack[stack.length - 1];
+            if(s === "[" || s === "(" || s === "{"){
+                stack.push(s);
+            } else if((s === "]" && l === "[") 
+                      || (s === ")" && l === "(") 
+                      || (s === "}" && l === "{")){
+                stack.pop(); 
+            } else {
                 break;
             }
+            if(j === size - 1 && stack.length === 0) {
+                answer++;
+            }
         }
-        if(!stop && stack.length === 0) answer++;
-        
-        // 왼쪽으로 한 칸 회전
-        arr.push(arr.shift());
+        row.push(row.shift());
     }
     return answer;
 }
-
-
-
-// }]()[{ X
-// ]()[{} X
-// ()[{}] O
-// )[{}]( X
-// [{}]() O
-// {}]()[ X
