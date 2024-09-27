1a377b13095d25a598e90d05229c3a489f1ad244
YiRyung <sryung1225@gmail.com>
2024-09-27 12:05:02 +0900

    [level 2] Title: 주식가격, Time: 39.68 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,22 +1,19 @@
 function solution(prices) {
-    const answer = new Array(prices.length).fill(0);
-    const stack = []; // 하락하지 않은 가격의 인덱스들
+    const sec = prices.length;
+    let answer = new Array(sec).fill(0); // 가격이 떨어지지 않은 기간을 저장할 배열
     
-    for(let i=0; i<prices.length; i++){
-        const price = prices[i]; // 현재 가격
-        // 아직 하락하지 않는 가격들이 있으면서
-        // 그 가격들이 현재 가격보다 높은 가격일 때 (= 지금 하락했을 때)
+    const stack = []; // 이전/현재 가격을 비교할 스택
+    prices.forEach((price, index) => {
         while(stack.length > 0 && prices[stack[stack.length - 1]] > price){
-            const idx = stack.pop();
-            answer[idx] = i - idx; // 현재 인덱스 - 하락 시작 인덱스 (= 가격 유지 시간)
+            // 가격 하락 감지
+            const i = stack.pop();
+            answer[i] += index - i; // 가격 유지 기간 계산
         }
-        stack.push(i); // 현재 인덱스 저장
-    }
+        stack.push(index);
+    })
+
+    // 하락 없이 계속 유지한 가격들의 유지 기간 계산
+    answer = answer.map((num, index) => num ? num : sec - index - 1);
     
-    // 스택에 남은 인덱스 = 끝까지 가격을 방어한 인덱스들
-    while (stack.length > 0) {
-        const idx = stack.pop();
-        answer[idx] = prices.length - 1 - idx; // 최대 시간 부여
-    }
     return answer;
 }
