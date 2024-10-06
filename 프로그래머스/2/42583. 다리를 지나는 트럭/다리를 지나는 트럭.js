8447ed0e2cdc2c0a4c809122f69b877aaa4ce61f
YiRyung <sryung1225@gmail.com>
2024-10-06 17:28:51 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,40 +1,21 @@
-class Queue {
-    items = [];
-    front = 0;
-    rear = 0;
-    sum = 0;
-    size() {
-        return this.rear - this.front;
-    }
-    pop() {
-        this.sum -= this.items[this.front];
-        return this.items[this.front++];
-    }
-    push(item) {
-        this.sum += item;
-        this.rear++
-        this.items.push(item);
-    }
-}
-
 function solution(bridge_length, weight, truck_weights) {
     let answer = 0;
+    let bridgeQueue = [[0, 0]];
+    let weightOnBridge = 0;
     
-    let truckIndex = 0;
-    const bridgeQueue = new Queue();
-    
-    while(truckIndex <= truck_weights.length - 1 || bridgeQueue.sum !== 0){
-        answer++;
-        if(bridgeQueue.size() >= bridge_length){
-           bridgeQueue.pop(); 
+    while(bridgeQueue.length > 0 || truck_weights.length > 0){
+        if(bridgeQueue[0][1] === answer){
+            weightOnBridge -= bridgeQueue.shift()[0];
         }
-        const truck = truck_weights[truckIndex];
-        if(truck + bridgeQueue.sum <= weight) {
-            truckIndex++;
-            bridgeQueue.push(truck);
+        if(weightOnBridge + truck_weights[0] <= weight){
+            weightOnBridge += truck_weights[0];
+            bridgeQueue.push([truck_weights.shift(), answer + bridge_length]);
         } else {
-            bridgeQueue.push(0);
+            if (bridgeQueue[0]) {
+                answer = bridgeQueue[0][1] - 1;
+            }
         }
+        answer++;
     }
     return answer;
 }
