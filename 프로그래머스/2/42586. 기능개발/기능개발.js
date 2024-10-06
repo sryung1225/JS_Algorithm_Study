b0c0756a49bc1718f2d83e5a937b76e5d9668e2e
YiRyung <sryung1225@gmail.com>
2024-10-06 16:25:31 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,11 +1,12 @@
 function solution(progresses, speeds) {
     const size = progresses.length;
     const answer = [];
+    const daysLeft = [...progresses].map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
     for(let i=0; i<size; i++){
-        const days = Math.ceil((100 - progresses[i]) / speeds[i]);
+        const maxDays = daysLeft[i];
         let count = 1;
         for(let j=i+1; j<size; j++){
-            if(progresses[j] + (speeds[j] * days) >= 100) count++;
+            if(maxDays >= daysLeft[j]) count++;
             else break;
         }
         answer.push(count);
