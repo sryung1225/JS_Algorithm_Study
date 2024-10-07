31bf48d100c0299aeaf9f4653b3804fde87bbb44
YiRyung <sryung1225@gmail.com>
2024-10-08 00:01:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.51 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,12 @@
 function solution(participant, completion) {
-    const obj = {};
-    for (const p of participant){
-        obj[p] = obj[p] ? obj[p] + 1 : 1; 
+    const map = new Map(); // map.set(key, value)
+    for (const i in participant) {
+        const p = participant[i];
+        const c = completion[i];
+        map.set(p, (map.get(p) || 0) + 1);
+        map.set(c, (map.get(c) || 0) - 1);        
     }
-    for (const c of completion){
-        if(obj[c] > 0) obj[c]--;
-    }
-    for (const key in obj) {
-        if(obj[key] > 0) return key;
+    for (const [key, value] of map) {
+        if(value > 0) return key;
     }
 }
