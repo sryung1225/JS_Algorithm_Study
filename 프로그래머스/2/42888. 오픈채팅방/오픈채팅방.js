2e1b7964daafe4202414dba84578302d647c22ba
YiRyung <sryung1225@gmail.com>
2024-10-10 16:58:46 +0900

    [level 2] Title: 오픈채팅방, Time: 252.24 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,22 +1,18 @@
 function solution(record) {
     const user = {};
-    const recordArr = [];
+    const answer = [];
     // 유저 uid 변경 감지
     record.forEach((reco) => {
         const [action, uid, name] = reco.split(" ");
         if(action === "Enter" || action === "Change")
             user[uid] = name;
         if(action === "Enter" || action === "Leave")
-            recordArr.push([action, uid]);
+            answer.push([action, uid]);
     })
     // 메세지 작성
-    const answer = [];
-    recordArr.forEach((reco) => {
-        const [action, uid] = reco;
-        if(action === "Enter")
-            answer.push(`${user[uid]}님이 들어왔습니다.`);
-        else if(action === "Leave")
-            answer.push(`${user[uid]}님이 나갔습니다.`);
+    return answer.map((value) => {
+        const [action, uid] = value;
+        if(action === "Enter") return `${user[uid]}님이 들어왔습니다.`;
+        else if(action === "Leave") return `${user[uid]}님이 나갔습니다.`;
     });
-    return answer;
 }
