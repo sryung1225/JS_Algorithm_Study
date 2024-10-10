67fda1c3c815c99606951581b1fc2dc96e907922
YiRyung <sryung1225@gmail.com>
2024-10-10 16:32:43 +0900

    [level 2] Title: 할인 행사, Time: 35.99 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#) 
 
 ### 성능 요약
 
-메모리: 41 MB, 시간: 27.87 ms
+메모리: 42 MB, 시간: 35.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 10일 15:54:19
+2024년 10월 10일 16:32:40
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,21 +1,36 @@
 function solution(want, number, discount) {
     // 구매하고 싶은 물품과 갯수 할당
-    const obj = {};
+    const wantObj = {}
     want.forEach((item, index) => {
-        obj[item] = number[index];
+        wantObj[item] = number[index];
     })
+    const wantKey = Object.keys(wantObj);
     
-    // 10일치 우선 계산
-    discount.slice(0, 10).forEach((item) => {
-        if(obj[item] !== undefined) obj[item]--;
-    });
+    // 할인 첫날부터 10일 동안의 할인 물품과 갯수 할당
+    const discountObj = {}
+    discount.slice(0, 10).forEach((item, index) => {
+        discountObj[item] = (discountObj[item] || 0) + 1;
+    })
     
     // 순환하면서 계산 진행. 모든 항목을 구매할 수 있는 날 계산
     let answer = 0;
-    for(let i=0; i<=discount.length - 10; i++){
-        if(Object.values(obj).every((value) => value <= 0)) answer++;
-        if(obj[discount[i]] !== undefined) obj[discount[i]]++; // 할인에서 제외
-        if(obj[discount[i+10]] !== undefined) obj[discount[i+10]]--; // 할인에서 추가
+    for(let i=0; i<=discount.length-10; i++) {
+        let check = true;
+        for(const key of wantKey) {
+            if((discountObj[key] || 0) < wantObj[key]){
+                check = false;
+                break;
+            }
+        }
+        if(check) answer++;
+        
+        const prev = discount[i];
+        if(discountObj[prev] !== undefined) {
+            discountObj[prev]--;
+            if(discountObj[prev] === 0) delete discountObj[prev];
+        }
+        const next = discount[i+10];
+        discountObj[next] = (discountObj[next] || 0) + 1;
     }
     return answer;
 }
