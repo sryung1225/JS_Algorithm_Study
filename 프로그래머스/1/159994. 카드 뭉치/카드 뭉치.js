f0a1b23ff1fcbd7dbb961ed0f493c42f1a4cded2
YiRyung <sryung1225@gmail.com>
2024-10-10 14:26:43 +0900

    [level 1] Title: 카드 뭉치, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,37 @@
-function solution(cards1, cards2, goal) {
-    let answer = "Yes";
-    let [index1, index2] = [0, 0];
-    for(let word of goal){
-        if(word === cards1[index1]) index1++;
-        else if(word === cards2[index2]) index2++;
-        else {
-            answer = "No";
-            break;
-        }
+class Queue {
+    items = [];
+    front = 0;
+    rear = 0;
+    constructor(array) {
+        this.items = array;
+        this.rear = array.length;
     }
-    return answer;
+    pop() {
+        return this.items[this.front++];
+    }
+    first() {
+        return this.items[this.front];
+    }
+    isEmpty() {
+        return this.front === this.rear;
+    }
+}
+
+function solution(cards1, cards2, goal) {
+    const cardsQueue1 = new Queue(cards1);
+    const cardsQueue2 = new Queue(cards2);
+    const goalQueue = new Queue(goal);
+    
+    while(!goalQueue.isEmpty()){
+        if(!cardsQueue1.isEmpty() && cardsQueue1.first() === goalQueue.first()){
+            cardsQueue1.pop();
+            goalQueue.pop();
+        } else if(!cardsQueue2.isEmpty() && cardsQueue2.first() === goalQueue.first()){
+            cardsQueue2.pop();
+            goalQueue.pop();
+         } else {
+             return "No";
+         }
+    }
+    return "Yes";
 }
