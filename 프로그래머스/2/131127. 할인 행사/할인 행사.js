b3a425ee1089a950215fad3834781b9ffb7c2701
YiRyung <sryung1225@gmail.com>
2024-10-10 15:54:20 +0900

    [level 2] Title: 할인 행사, Time: 27.87 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,19 +1,21 @@
 function solution(want, number, discount) {
+    // 구매하고 싶은 물품과 갯수 할당
+    const obj = {};
+    want.forEach((item, index) => {
+        obj[item] = number[index];
+    })
+    
+    // 10일치 우선 계산
+    discount.slice(0, 10).forEach((item) => {
+        if(obj[item] !== undefined) obj[item]--;
+    });
+    
+    // 순환하면서 계산 진행. 모든 항목을 구매할 수 있는 날 계산
     let answer = 0;
     for(let i=0; i<=discount.length - 10; i++){
-        const obj = {};
-        for(let j=0; j<10; j++){
-            if(!obj[discount[i+j]]) obj[discount[i+j]] = 1;
-            else obj[discount[i+j]]++;
-        }
-        let can = true;
-        for(let k=0; k<want.length; k++){
-            if(!obj[want[k]] || obj[want[k]] < number[k]){
-                can = false;
-                break;
-            }
-        }
-        if(can) answer++;
+        if(Object.values(obj).every((value) => value <= 0)) answer++;
+        if(obj[discount[i]] !== undefined) obj[discount[i]]++; // 할인에서 제외
+        if(obj[discount[i+10]] !== undefined) obj[discount[i+10]]--; // 할인에서 추가
     }
     return answer;
 }
