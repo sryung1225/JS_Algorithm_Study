eb791665b1c84415a3c6b1d369eff7500a155d5c
YiRyung <sryung1225@gmail.com>
2024-10-13 18:14:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,26 @@
+function solution(genres, plays) {
+    const playsArr = genres.map((genre, index) => [genre, plays[index], index]);
+    
+    const genresObj = {};
+    genres.forEach((genre, index) => {
+        genresObj[genre] = (genresObj[genre] || 0) + plays[index];
+    })
+    const bestGenres = Object.entries(genresObj).sort((a, b) => a[1] - b[1]).map((genre) => genre[0]);
+    
+    const answer = [];
+    while(bestGenres.length > 0){
+        const genre = bestGenres.pop();
+        const matchGenre = playsArr.filter((play) => play[0] === genre).sort((a, b) => {
+            if(a[1] < b[1]) return 1;
+            else if(a[1] > b[1]) return -1;
+            else {
+                if(a[2] > b[2]) return 1;
+                else return -1;
+            }
+        })
+        for(let i=0; i<Math.min(2, matchGenre.length); i++){
+            answer.push(matchGenre[i][2]);
+        }
+    }
+    return answer;
+}
