71d905395870f1823919536c1ff10f66fdc1f16b
YiRyung <sryung1225@gmail.com>
2024-10-13 23:38:19 +0900

    [level 1] Title: 신고 결과 받기, Time: 371.95 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -2,17 +2,14 @@ function solution(id_list, report, k) {
     const reportObj = {};
     report.forEach((rep) => {
         const [from, to] = rep.split(" ");
-        if(!reportObj[to]) reportObj[to] = [0, new Set()];
-        if(!reportObj[to][1].has(from)){
-            reportObj[to][0]++;
-            reportObj[to][1].add(from);
-        }
+        if(!reportObj[to]) reportObj[to] = new Set();
+        reportObj[to].add(from);
     })
-    const reportArr = Object.entries(reportObj);
+    const reportArr = Object.keys(reportObj);
     let answer = new Array(id_list.length).fill(0);
     reportArr.forEach((rep) => {
-        if(rep[1][0] >= k){
-            [...rep[1][1]].forEach((id) => {
+        if(reportObj[rep].size >= k){
+            [...reportObj[rep]].forEach((id) => {
                 answer[id_list.indexOf(id)]++;
             })
         }
