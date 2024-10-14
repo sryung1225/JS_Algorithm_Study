e4c96258c938b754a55a1a061c8f194c0089b12b
YiRyung <sryung1225@gmail.com>
2024-10-14 17:36:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 24.36 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,42 @@
+class Queue {
+    items = [];
+    front = 0;
+    rear = 0;
+    pop() {
+        return this.items[this.front++];
+    }
+    push(item) {
+        this.items.push(item);
+        this.rear++;
+    }
+    isEmpty() {
+        return this.front === this.rear;
+    }
+}
+
 function solution(maps) {
     const [n, m] = [maps.length, maps[0].length];
     const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
     
-    // 최단 거리? => BFS => 큐
-    const queue = [[0, 0, 1]]; // 방문 예정 위치 저장 (다음에 가야할 곳)
-    const visited = Array.from(Array(n), () => Array(m).fill(false)); // 방문 이력 저장 (n*m)
+    const queue = new Queue();
+    queue.push([0, 0, 1]); // [x좌표, y좌표, 거리]
+    const visited = Array.from(Array(n), () => Array(m).fill(false));
     visited[0][0] = true;
     
-    while(queue.length > 0){
-        const [x, y, dist] = queue.shift(); // 현재 위치
-        
-        // 상대방 진영 도착
-        if(x === n-1 && y === m-1){
-            return dist;
-        }
-        
-        for(const [dx, dy] of directions) {
-            const [nx, ny] = [x+dx, y+dy]; // 이동 예정 좌표
-            // 이동할 곳이 맵 내부라면
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-                // 이동할 곳이 방문 가능하고 이전에 방문한 적이 없다면
-                if(maps[nx][ny] === 1 && !visited[nx][ny]) {
-                    queue.push([nx, ny, dist+1]);
-                    visited[nx][ny] = true;
-                }
+    let answer = -1;
+    while(!queue.isEmpty() && answer === -1){    
+        const [x, y, dist] = queue.pop();
+        directions.forEach(([dx, dy]) => {
+            const [nx, ny] = [x + dx, y + dy];
+            if(nx === n - 1 && ny === m - 1) {
+                answer = dist + 1;
+                return;
             }
-        }
+            if(nx < n && nx >= 0 && ny < m && ny >= 0 && maps[nx][ny] === 1 && !visited[nx][ny]) {
+                queue.push([nx, ny, dist + 1]);
+                visited[nx][ny] = true;
+            }
+        })
     }
-    
-    return -1;
+    return answer;
 }
