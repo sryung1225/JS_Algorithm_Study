cf5da68cc1cb8fec8c1397322f03260b64d715fd
YiRyung <sryung1225@gmail.com>
2024-10-16 20:13:09 +0900

    [level 2] Title: 타겟 넘버, Time: 40.12 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,15 @@
+// 모든 경우의 수 => DFS (스택 / 재귀함수)
+
 function solution(numbers, target) {
     let answer = 0;
-    
     function dfs(index, sum){
-        if(index < numbers.length) {
-            dfs(index + 1, sum + numbers[index]);
-            dfs(index + 1, sum - numbers[index]);
-        } else {
-            if(sum === target) answer++;
+        if(index === numbers.length){
+            if(target === sum) answer++;
+            return;
         }
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
-    
     dfs(0, 0);
-    
     return answer;
 }
