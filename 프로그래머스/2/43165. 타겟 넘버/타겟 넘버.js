836798a8ce8c1e94a942fc2807a15721628e97fc
YiRyung <sryung1225@gmail.com>
2024-10-16 20:25:00 +0900

    [level 2] Title: 타겟 넘버, Time: 72.13 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,16 @@
 function solution(numbers, target) {
     let answer = 0;
-    const stack = [];
-    stack.push([0, 0]); // [index, sum]
+    const stack = [[0, 0]]; // [index, sum]
     while(stack.length > 0){
         const [index, sum] = stack.pop();
         if(index === numbers.length){
             if(sum === target){
                 answer++;
             }
-        } else {
-            stack.push([index + 1, sum + numbers[index]]);
-            stack.push([index + 1, sum - numbers[index]]);   
+            continue;
         }
+        stack.push([index + 1, sum + numbers[index]]);
+        stack.push([index + 1, sum - numbers[index]]);
     }
     return answer;
 }
