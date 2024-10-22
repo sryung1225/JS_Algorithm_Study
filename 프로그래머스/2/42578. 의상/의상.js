d8b3cade8cb1bd588c70a94ba31c1140677f3905
YiRyung <sryung1225@gmail.com>
2024-10-22 16:38:52 +0900

    [level 2] Title: 의상, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,12 +1,11 @@
 function solution(clothes) {
     const types = {};
     clothes.forEach(([name, type]) => {
-        if(!types[type]) types[type] = [];
-        types[type].push(name);
+        types[type] = (types[type] || 0) + 1;
     })
     let answer = 1;
-    Object.entries(types).forEach(([_, name]) => {
-        answer *= (name.length + 1);
+    Object.values(types).forEach((count) => {
+        answer *= (count + 1);
     })
     return answer - 1;
 }
