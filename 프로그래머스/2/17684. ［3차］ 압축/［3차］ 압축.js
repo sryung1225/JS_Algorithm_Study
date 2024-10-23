aebd0d9cc0d4d418f907a7c0d6a0a816b8079c49
YiRyung <sryung1225@gmail.com>
2024-10-23 15:34:44 +0900

    [level 2] Title: [3차] 압축, Time: 1.91 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,25 +1,22 @@
 function solution(msg) {
     const dic = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     const answer = [];
-    let left = 0;
-    while (left < msg.length) {
+    
+    for(let left = 0; left < msg.length; left++){
         let right = left;
         let word = "";
         let [prev, next] = [0, 0];
-        while(next !== -1 && right < msg.length){
-            right++;
-            word = msg.slice(left, right);
+        for(; next !== -1 && right < msg.length; right++){
+            word = msg.slice(left, right + 1);
             [prev, next] = [next, dic.indexOf(word)];
         }
         if(next === -1){
             answer.push(prev);
             dic.push(word);
-            left = right - 1;
+            left = right - 2;
         } else if(next > 0 && right === msg.length){
             answer.push(next);
             break;
-        } else {
-            left++;
         }
     }
     return answer;
