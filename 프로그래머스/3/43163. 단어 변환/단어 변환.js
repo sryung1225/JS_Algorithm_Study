36c70a40b3b9d840aa94f5d4228f8e643336b38c
YiRyung <sryung1225@gmail.com>
2024-10-27 16:37:27 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,3 +1,21 @@
+class Queue {
+    constructor(initialItems = []) {
+        this.items = initialItems;
+        this.front = 0;
+        this.rear = initialItems.length;
+    }
+    push(item) {
+        this.items.push(item);
+        this.rear++;
+    }
+    pop() {
+        return this.items[this.front++];
+    }
+    isEmpty() {
+        return this.front === this.rear;
+    }
+}
+
 function isOneLetterDifferent(word1, word2){
     let diffCount = 0;
     for(let i=0; i<word1.length; i++){
@@ -8,12 +26,12 @@ function isOneLetterDifferent(word1, word2){
 }
 
 function solution(begin, target, words) {
-    const queue = [[begin, 0]];
+    const queue = new Queue([[begin, 0]]);
     let visited = new Set();
     visited.add(begin);
     
-    while(queue.length > 0){
-        const [currentWord, steps] = queue.shift();
+    while(!queue.isEmpty()){
+        const [currentWord, steps] = queue.pop();
         if(currentWord === target) return steps;
         for(let word of words){
             if(!visited.has(word) && isOneLetterDifferent(currentWord, word)){
