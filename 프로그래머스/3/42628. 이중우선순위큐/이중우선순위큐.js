d6def0b69ab2597caadecd9d7b1e647b96f0722c
YiRyung <sryung1225@gmail.com>
2024-11-08 17:35:50 +0900

    [level 3] Title: 이중우선순위큐, Time: 118.45 ms, Memory: 50.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,20 +1,17 @@
 function solution(operations) {
     let queue = [];
-    operations.forEach((oper) => {
-        switch(oper){
-            case "D 1":
-                const max = Math.max(...queue);
-                queue.splice(queue.indexOf(max), 1);  
-                break;
-            case "D -1":
-                const min = Math.min(...queue);
-                queue.splice(queue.indexOf(min), 1);
-                break;
-            default:
-                queue.push(Number(oper.split(" ")[1]));
+    operations.forEach((operation) => {
+        const [command, value] = operation.split(" ");
+        if(command === "I") {
+            queue.push(Number(value));
+        } else if(command === "D" && value === "1"){
+            const max = Math.max(...queue);
+            queue.splice(queue.indexOf(max), 1);  
+        } else if(command === "D" && value === "-1"){
+            const min = Math.min(...queue);
+            queue.splice(queue.indexOf(min), 1);
         }
     })
-    
     if(queue.length === 0) return [0, 0];
     return [Math.max(...queue), Math.min(...queue)];
 }
